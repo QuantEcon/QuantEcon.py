1ff1767633c7cff652e3c6975eee75e01803cee1
John Stachurski <john.stachurski@gmail.com>
2013-05-02 05:56:34 +1000

    Minor edits

--- dice.py
+++ dice.py
@@ -4,8 +4,8 @@ class Dice:
 
     faces = (1, 2, 3, 4, 5, 6)
 
-    def __init__(self, current_face):
-       self.current_face = current_face
+    def __init__(self):
+       self.current_face = 1
 
     def roll(self):
         self.current_face = random.choice(Dice.faces)
