c9dbfa61cf491a1ccea63a819ab6ce2e127d3203
John Stachurski <john.stachurski@gmail.com>
2014-05-27 15:11:58 +1000

    changed init file

--- quantecon/__init__.py
+++ quantecon/__init__.py
@@ -7,26 +7,26 @@
 		ap = qe.asset_pricing.AssetPrices()
 '''
 
-import asset_pricing
-import career
-import compute_fp
-import discrete_rv 			#ClassName is not PEP8
-import ecdf					#ClassName is not PEP8
-import estspec
-import ifp 					#ClassName is not PEP8
-import jv
-import kalman
-import lae
-import linproc 				#ClassName is not PEP8
-import lqramsey
-import lss
-import lucastree
-import mc_tools
-import odu_vfi
-import optgrowth
-import quadsums
-import rank_nullspace
-import riccati
-import robustlq
-import tauchen
+#import asset_pricing
+#import career
+#import compute_fp
+#import discrete_rv 			#ClassName is not PEP8
+#import ecdf					#ClassName is not PEP8
+#import estspec
+#import ifp 					#ClassName is not PEP8
+#import jv
+#import kalman
+#import lae
+#import linproc 				#ClassName is not PEP8
+#import lqramsey
+#import lss
+#import lucastree
+#import mc_tools
+#import odu_vfi
+#import optgrowth
+#import quadsums
+#import rank_nullspace
+#import riccati
+#import robustlq
+#import tauchen
 
