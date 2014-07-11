4578f561f8a276e2a9dd20c1f61f86672629f424
John Stachurski <john.stachurski@gmail.com>
2014-07-11 16:40:34 +1000

    added new exercise to statd, speed_solutions.ipynb

--- examples/boxplot_example.py
+++ examples/boxplot_example.py
@@ -1,11 +1,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
-x = np.random.randn(100)        # N(0, 1)
-y = np.random.randn(100) + 2.0  # N(2, 1)
-z = np.random.randn(100) + 4.0  # N(4, 1)
-x = np.exp(x)  # Turn x into a lognormal
-fig, ax = plt.subplots()
+
+n = 500
+x = np.random.randn(n)        # N(0, 1)
+x = np.exp(x)                 # Map x to lognormal
+y = np.random.randn(n) + 2.0  # N(2, 1)
+z = np.random.randn(n) + 4.0  # N(4, 1)
+
+fig, ax = plt.subplots(figsize=(10, 6.6))
 ax.boxplot([x, y, z])
+ax.set_xticks((1, 2, 3))
+ax.set_ylim(-2, 14)
+ax.set_xticklabels((r'$X$', r'$Y$', r'$Z$'), fontsize=16) 
 plt.show()
 
 
