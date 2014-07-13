4abfc443abcc0113a96af829ad25b304f5395098
John Stachurski <john.stachurski@gmail.com>
2014-07-13 14:01:27 +1000

    fixed docstrings on asset_pricing, career, compute_fp

--- quantecon/asset_pricing.py
+++ quantecon/asset_pricing.py
@@ -13,34 +13,72 @@ import numpy as np
 from numpy.linalg import solve
 
 class AssetPrices:
+    """
+    A class to compute asset prices when the endowment follows a finite Markov
+    chain.
+
+    References
+    ----------
+
+        http://quant-econ.net/markov_asset.html
+
+    Examples
+    --------
+
+        >>> n = 5
+        >>> P = 0.0125 * np.ones((n, n))
+        >>> P += np.diag(0.95 - 0.0125 * np.ones(5))
+        >>> s = np.array([1.05, 1.025, 1.0, 0.975, 0.95])
+        >>> gamma = 2.0
+        >>> beta = 0.94
+        >>> ap = AssetPrices(beta, P, s, gamma)
+        >>> zeta = 1.0
+        >>> v = ap.tree_price()
+        >>> print "Lucas Tree Prices: ", v
+
+        >>> v_consol = ap.consol_price(zeta)
+        >>> print "Consol Bond Prices: ", v_consol
+
+        >>> p_s = 150.0
+        >>> w_bar, w_bars = ap.call_option(zeta, p_s, T = [10,20,30])
+
+
+    """
+
 
     def __init__(self, beta, P, s, gamma):
-        '''
+        """
         Initializes an instance of AssetPrices
 
         Parameters
-        ==========
+        ----------
         beta : float
-            discount factor
+            Discount factor
 
         P : array_like
-            transition matrix
+            Transition matrix
 
         s : array_like
-            growth rate of consumption
+            Growth rate of consumption
 
         gamma : float
-            coefficient of risk aversion
-        '''
+            Coefficient of risk aversion
+
+        """
         self.beta, self.gamma = beta, gamma
         self.P, self.s = P, s
         self.n = self.P.shape[0]
 
     def tree_price(self):
-        '''
+        """
         Computes the function v such that the price of the lucas tree is
         v(lambda)C_t
-        '''
+
+        Returns
+        -------
+        v : np.ndarray
+            Lucas tree prices
+        """
         # == Simplify names == #
         P, s, gamma, beta = self.P, self.s, self.gamma, self.beta
         # == Compute v == #
@@ -51,15 +89,20 @@ class AssetPrices:
         return v
 
     def consol_price(self, zeta):
-        '''
+        """
         Computes price of a consol bond with payoff zeta
 
         Parameters
-        ===========
+        ----------
         zeta : float
-            coupon of the console
+            Coupon of the console
 
-        '''
+        Returns
+        -------
+        p_bar : np.ndarray
+            Console bond prices
+
+        """
         # == Simplify names == #
         P, s, gamma, beta = self.P, self.s, self.gamma, self.beta
         # == Compute price == #
@@ -70,23 +113,34 @@ class AssetPrices:
         return p_bar
 
     def call_option(self, zeta, p_s, T=[], epsilon=1e-8):
-        '''
-        Computes price of a call option on a consol bond with payoff zeta
+        """
+        Computes price of a call option on a consol bond, both finite and
+        infinite horizon
 
         Parameters
-        ===========
+        ----------
         zeta : float
-            coupon of the console
+            Coupon of the console
 
         p_s : float
-            strike price
+            Strike price
 
         T : list of integers
-            length of option
+            Length of option in the finite horizon case
 
-        epsilon : float
-            tolerance for infinite horizon problem
-        '''
+        epsilon : float, optional
+            Tolerance for infinite horizon problem
+
+        Returns
+        -------
+        w_bar : np.ndarray
+            Infinite horizon call option prices
+
+        w_bars : dict
+            A dictionary of key-value pairs {t: vec}, where t is one of the
+            dates in the list T and vec is the option prices at that date
+
+        """
         # == Simplify names, initialize variables == #
         P, s, gamma, beta = self.P, self.s, self.gamma, self.beta
         P_check = P * s**(-gamma)
--- quantecon/career.py
+++ quantecon/career.py
@@ -2,7 +2,16 @@
 Filename: career.py
 Authors: Thomas Sargent, John Stachurski 
 
-A collection of functions to solve the career / job choice model of Neal.
+A collection of functions to solve the career / job choice model due to Derek Neal.
+
+References
+----------
+
+  http://quant-econ.net/career.html
+
+..  [Neal1999] Neal, D. (1999). The Complexity of Job Mobility among Young Men, 
+               Journal of Labor Economics, 17(2), 237-261.
+
 """
 
 import numpy as np
@@ -11,8 +20,30 @@ from scipy.special import binom, beta
 
 def gen_probs(n, a, b):
     """
-    Generate and return the vector of probabilities for the Beta-binomial 
+    Generate the vector of probabilities for the Beta-binomial 
     (n, a, b) distribution.
+
+    The Beta-binomial distribution takes the form
+
+    .. math::
+        p(k \,|\, n, a, b) 
+        = {n \choose k} \frac{B(k + a, n - k + b)}{B(a, b)},
+        \qquad k = 0, \ldots, n
+
+    Parameters
+    ----------
+    n : int
+        First parameter to the Beta-binomial distribution
+    a : float
+        Second parameter to the Beta-binomial distribution
+    b : float
+        Third parameter to the Beta-binomial distribution
+
+    Returns
+    -------
+    probs: np.ndarray
+        Vector of probabilities over k
+
     """
     probs = np.zeros(n+1)
     for k in range(n+1):
@@ -21,8 +52,32 @@ def gen_probs(n, a, b):
 
 
 class CareerWorkerProblem:
+    """
+    An instance of the class is an object with data on a particular problem of
+    this type, including probabilites, discount factor and sample space for
+    the variables.
+    """
 
     def __init__(self, B=5.0, beta=0.95, N=50, F_a=1, F_b=1, G_a=1, G_b=1):
+        """
+        Parameters
+        ----------
+        beta : float, optional
+            Discount factor
+        B : float, optional
+            Upper bound of for both epsilon and theta
+        N : int, optional
+            Number of possible realizations for both epsilon and theta
+        F_a : int or float, optional
+            Parameter `a` from the career distribution 
+        F_b : int or float, optional
+            Parameter `b` from the career distribution 
+        G_a : int or float, optional
+            Parameter `a` from the job distribution 
+        G_b : int or float, optional
+            Parameter `b` from the job distribution 
+
+        """
         self.beta, self.N, self.B = beta, N, B
         self.theta = np.linspace(0, B, N)     # set of theta values
         self.epsilon = np.linspace(0, B, N)   # set of epsilon values
@@ -34,14 +89,18 @@ class CareerWorkerProblem:
     def bellman(self, v):
         """
         The Bellman operator for the career / job choice model of Neal.  
-        
-            * v is a 2D NumPy array representing the value function
-            
-        The array v should be interpreted as 
-        
-            v[i, j] = v(theta_i, epsilon_j).  
 
-        Returns updated value function Tv as an array of shape v.shape
+        Parameters
+        ----------
+            v : np.ndarray
+                A 2D NumPy array representing the value function
+                Interpretation: :math:`v[i, j] = v(\theta_i, \epsilon_j)`
+
+        Returns
+        -------
+            new_v : np.ndarray
+                The updated value function Tv as an array of shape v.shape
+
         """
         new_v = np.empty(v.shape)
         for i in range(self.N):
@@ -56,12 +115,23 @@ class CareerWorkerProblem:
 
     def get_greedy(self, v):
         """
-        Compute optimal actions taking v as the value function.  Parameters 
-        are the same as for bellman().  Returns a 2D NumPy array "policy", 
-        where policy[i, j] is the optimal action at (theta_i, epsilon_j).  
+        Compute optimal actions taking v as the value function.  
+        
+        Parameters
+        ----------
+            v : np.ndarray
+                A 2D NumPy array representing the value function
+                Interpretation: :math:`v[i, j] = v(\theta_i, \epsilon_j)`
 
-        The optimal action is represented as an integer in the set 1, 2, 3, 
-        where 1 = 'stay put', 2 = 'new job' and 3 = 'new life'
+        Returns
+        -------
+            policy : np.ndarray
+                A 2D NumPy array, where policy[i, j] is the optimal action 
+                at :math:`(\theta_i, \epsilon_j)`.  
+
+                The optimal action is represented as an integer in the set 1,
+                2, 3, where 1 = 'stay put', 2 = 'new job' and 3 = 'new life'
+
         """
         policy = np.empty(v.shape, dtype=int)
         for i in range(self.N):
