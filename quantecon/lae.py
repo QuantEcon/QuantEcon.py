af9ad849e93d16581d9f319a1d5fc324f4543c74
John Stachurski <john.stachurski@gmail.com>
2014-07-13 18:56:18 +1000

    cleaned up docstring of lae.py

--- quantecon/lae.py
+++ quantecon/lae.py
@@ -3,10 +3,11 @@ Filename: lae.py
 Authors: Thomas J. Sargent, John Stachurski,
 
 Computes a sequence of marginal densities for a continuous state space Markov
-chain {X_t} where the transition probabilities can be represented as densities.
-The estimate of the marginal density psi_t of X_t is
+chain X_t where the transition probabilities can be represented as densities.
+The estimate of the marginal density of X_t is
 
-    (1/n) sum_{i=0}^n p(X_{t-1}^i, y)
+.. math::
+    (1/n) \sum_{i=0}^n p(X_{t-1}^i, y)
 
 This is a density in y.  
 
@@ -17,23 +18,27 @@ import numpy as np
 class LAE:
     """
     An instance is a representation of a look ahead estimator associated with
-    a given stochastic kernel p and a vector of observations X.  For example,
+    a given stochastic kernel p and a vector of observations X.  
 
-        >>> psi = LAE(p, X)
-        >>> y = np.linspace(0, 1, 100)
-        >>> psi(y)  # Evaluate look ahead estimate at grid of points y
+    Example
+    -------
+
+    >>> psi = LAE(p, X)
+    >>> y = np.linspace(0, 1, 100)
+    >>> psi(y)  # Evaluate look ahead estimate at grid of points y
+
     """
 
     def __init__(self, p, X):
         """
         Parameters
-        ==========
+        ----------
         p : function
             The stochastic kernel.  A function p(x, y) that is vectorized in
             both x and y
-
         X : array_like
             A vector containing observations
+
         """
         X = X.flatten()  # So we know what we're dealing with
         n = len(X)
@@ -42,14 +47,17 @@ class LAE:
 
     def __call__(self, y):
         """
+        A vectorized function that returns the value of the look ahead
+        estimate at the values in the array y.
+
         Parameters
-        ==========
+        ----------
         y : array_like
             A vector of points at which we wish to evaluate the look-ahead
             estimator
 
         Returns
-        =======
+        -------
         psi_vals : numpy.ndarray
             The values of the density estimate at the points in y
 
