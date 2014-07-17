c4147886d634d65f28bee9c1a764f7110e55becb
David R. Pugh <drobert.pugh@gmail.com>
2014-07-16 23:25:28 -0400

    Added equations for Solow model with CES production technology.

--- quantecon/solow.py
+++ quantecon/solow.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 import ivp
 
 
@@ -11,13 +13,13 @@ class Model(ivp.IVP):
         Arguments
         ----------
         k_dot : callable, ``k_dot(t, k, params)``
-            Equation of motion for capital (per person/effective person). The
-            independent variable, `t`, is time; `k`, is capital (per person/
-            effective person); `params` is a dictionary of model parameters.
+            Equation of motion for capital (per worker/effective worker). The
+            independent variable, `t`, is time; `k`, is capital (per worker/
+            effective worker); `params` is a dictionary of model parameters.
         jacobian : callable, ``jacobian(t, k, params)``
-            The derivative of the equation of motion for capital (per person/
-            effective person) with respect to `k`. The independent variable, t,
-            is time; k, (per person/effective person); `params` is a dictionary
+            The derivative of the equation of motion for capital (per worker/
+            effective worker) with respect to `k`. The independent variable, t,
+            is time; k, (per worker/effective worker); `params` is a dictionary
             of model parameters.
         params : dict
             Dictionary of model parameters. Standard parameters for a Solow
@@ -36,6 +38,143 @@ class Model(ivp.IVP):
         super(Model, self).__init__(k_dot, jacobian, (params,))
 
 
+def ces_actual_investment(k, s, alpha, sigma):
+    """
+    Total amount of output (per worker/effective worker) invested into the
+    production of new capital.
+
+    Arguments
+    ---------
+    k : array_like (float)
+        Capital (per worker/effective worker).
+    s : float
+        Savings rate. Must satisfy ``0 < s < 1``.
+    alpha : float
+        Importance of capital relative to effective labor in production. Must
+        satisfy :math:`0 < \alpha < 1`.
+    sigma : float
+        Elasticity of substitution between capital and effective labor in
+        production. Must satisfy :math:`0 \le \sigma`.
+
+    Returns
+    -------
+    actual_investment : array_like (float)
+        Total amount of output (per worker/effective worker) invested into the
+        production of new capital.
+
+    """
+    actual_investment = s * ces_output(k, 1, 1, alpha, 1-alpha, sigma)
+    return actual_investment
+
+
+def ces_break_even_investment(k, g, n, delta):
+    """
+    Amount of investment required to maintain current levels of capital (per
+    worker/effective worker).
+
+    Arguments
+    ---------
+    k : array_like (float)
+        Capital (per worker/effective worker).
+    g : float
+        Growth rate of technology.
+    n : float
+        Growth rate of the labor force.
+    delta : float
+        Depreciation rate of physical capital. Must satisfy
+        :math:`0 < \delta`.
+
+    Returns
+    -------
+    break_even_investment : array_like (float)
+        Amount of investment required to maintain current levels of capital
+        (per worker/effective worker).
+
+    """
+    break_even_investment = (n + g + delta) * k
+    return break_even_investment
+
+
+def ces_k_dot(k, t, g, n, s, alpha, delta, sigma):
+    """
+    Equation of motion for capital (per worker/effective worker) for a
+    Solow growth model with constant elasticity of substitution (CES)
+    production function.
+
+    Arguments
+    ---------
+    k : array_like (float)
+        Capital (per worker/effective worker).
+    t : array_like (float)
+        Time.
+    g : float
+        Growth rate of technology.
+    n : float
+        Growth rate of the labor force.
+    s : float
+        Savings rate. Must satisfy ``0 < s < 1``.
+    alpha : float
+        Importance of capital relative to effective labor in production. Must
+        satisfy :math:`0 < \alpha < 1`.
+    delta : float
+        Depreciation rate of physical capital. Must satisfy
+        :math:`0 < \delta`.
+    sigma : float
+        Elasticity of substitution between capital and effective labor in
+        production. Must satisfy :math:`0 \le \sigma`.
+
+    Returns
+    -------
+    k_dot : array_like (float)
+        Rate of change of capital (per worker/effective worker).
+
+    """
+    k_dot = (ces_actual_investment(k, s, alpha, sigma) -
+             ces_break_even_investment(k, g, n, delta))
+    return k_dot
+
+
+def ces_output(K, A, L, alpha, beta, sigma):
+    """
+    Constant elasticity of substitution (CES) production function with labor
+    augmenting technology.
+
+    Arguments
+    ---------
+    K : array_like (float)
+        Capital
+    L : array_like (float)
+        Labor
+    A : array_like (float)
+        Technology
+    alpha : float
+        Importance of capital in production. Must satisfy :math:`0 < \alpha`.
+    beta : float
+        Importance of effective labor in production. Must satisfy
+        :math:`0 < \beta`.
+    sigma : float
+        Elasticity of substitution between capital and effective labor in
+        production. Must satisfy :math:`0 \le \sigma`.
+
+    Returns
+    -------
+    Y : array_like (float)
+        Output
+
+    """
+    rho = (sigma - 1) / sigma
+
+    # CES nests both Cobb-Douglas and Leontief functions
+    if abs(rho) < 1e-3:
+        Y = K**alpha * (A * L)**beta
+    elif sigma < 1e-3:
+        Y = np.minimum(alpha * K, beta * A * L)
+    else:
+        Y = (alpha * K**rho + beta * (A * L)**rho)**(1 / rho)
+
+    return Y
+
+
 def cobb_douglas_analytic_solution(k0, t, g, n, s, alpha, delta):
     """
     Analytic solution for the Solow model with Cobb-Douglas production.
@@ -43,7 +182,7 @@ def cobb_douglas_analytic_solution(k0, t, g, n, s, alpha, delta):
     Arguments
     ---------
         k0 : float
-            Initial value for capital (per person/effective person)
+            Initial value for capital (per worker/effective worker)
         t : array_like (float, shape=(T,))
             Array of points at which the solution is desired.
         g : float
@@ -69,10 +208,10 @@ def cobb_douglas_analytic_solution(k0, t, g, n, s, alpha, delta):
     lmbda = (n + g + delta) * (1 - alpha)
 
     # analytic solution for Solow model at time t
-    k_t   = (((s / (n + g + delta)) * (1 - np.exp(-lmbda * t)) +
-              k0**(1 - alpha) * np.exp(-lmbda * t))**(1 / (1 - alpha)))
+    k_t = (((s / (n + g + delta)) * (1 - np.exp(-lmbda * t)) +
+           k0**(1 - alpha) * np.exp(-lmbda * t))**(1 / (1 - alpha)))
 
     # combine into a (T, 2) array
-    analytic_traj = np.hstack((t[:,np.newaxis], k_t[:,np.newaxis]))
+    analytic_traj = np.hstack((t[:, np.newaxis], k_t[:, np.newaxis]))
 
     return analytic_traj
