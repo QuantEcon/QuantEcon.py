4cf5c323816c77316355e31b80be4e30dc664080
David R. Pugh <drobert.pugh@gmail.com>
2014-07-17 10:24:57 -0400

    Added placeholder methods for each of the functions.

--- quantecon/tests/test_ces.py
+++ quantecon/tests/test_ces.py
@@ -7,7 +7,19 @@ import ..ces
 
 
 class CESTestSuite(unittest.TestCase):
-    pass
+
+    def test_output(self):
+        """Test function for CES output."""
+        raise NotImplementedError
+
+    def test_marginal_product_capital(self):
+        """Test function for CES marginal product of capital."""
+        raise NotImplementedError
+
+    def test_marginal_product_labor(self):
+        """Test function for CES marginal product of labor."""
+        raise NotImplementedError
+
 
 if __name__ == '__main__':
     CESTest = unittest.TestLoader().loadTestsFromTestCase(CESTestSuite)
