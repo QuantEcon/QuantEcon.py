42b4784dcb24819d1a1094213fdd5cf3ddaae0ea
David R. Pugh <drobert.pugh@gmail.com>
2014-07-17 10:41:17 -0400

    Added output elasticities.

--- quantecon/ces.py
+++ quantecon/ces.py
@@ -126,4 +126,85 @@ def output(K, A, L, alpha, beta, sigma):
         Y = (alpha * K**rho + beta * (A * L)**rho)**(1 / rho)
 
     return Y
-    
+
+
+def output_elasticity_capital(K, A, L, alpha, beta, sigma):
+    """
+    Elasticity of output with respect to capital for the constant elasticity
+    of substitution (CES) production function with labor augmenting technology.
+
+    Arguments
+    ---------
+    K : array_like (float)
+        Capital
+    A : array_like (float)
+        Technology
+    L : array_like (float)
+        Labor
+    alpha : float
+        Importance of capital in production. Must satisfy :math:`0 < \alpha`.
+    beta : float
+        Importance of effective labor in production. Must satisfy
+        :math:`0 < \beta`.
+    sigma : float
+        Elasticity of substitution between capital and effective labor in
+        production. Must satisfy :math:`0 \le \sigma`.
+
+    Returns
+    -------
+    epsilon_YK : array_like (float)
+        Output elasticity with respect to capital.
+
+    """
+    rho = (sigma - 1) / sigma
+
+    # CES nests both Cobb-Douglas and Leontief functions
+    if abs(rho) < 1e-3:
+        epsilon_YK = alpha
+    elif sigma < 1e-3:
+        raise NotImplementedError
+    else:
+        epsilon_YK = alpha * K**rho / (alpha * K**rho + beta * (A * L)**rho)
+
+    return epsilon_YK
+
+
+def output_elasticity_labor(K, A, L, alpha, beta, sigma):
+    """
+    Elasticity of output with respect to labor for the constant elasticity
+    of substitution (CES) production function with labor augmenting technology.
+
+    Arguments
+    ---------
+    K : array_like (float)
+        Capital
+    A : array_like (float)
+        Technology
+    L : array_like (float)
+        Labor
+    alpha : float
+        Importance of capital in production. Must satisfy :math:`0 < \alpha`.
+    beta : float
+        Importance of effective labor in production. Must satisfy
+        :math:`0 < \beta`.
+    sigma : float
+        Elasticity of substitution between capital and effective labor in
+        production. Must satisfy :math:`0 \le \sigma`.
+
+    Returns
+    -------
+    epsilon_YL : array_like (float)
+        Output elasticity with respect to labor.
+
+    """
+    rho = (sigma - 1) / sigma
+
+    # CES nests both Cobb-Douglas and Leontief functions
+    if abs(rho) < 1e-3:
+        epsilon_YL = beta
+    elif sigma < 1e-3:
+        raise NotImplementedError
+    else:
+        epsilon_YL = beta * (A * L)**rho / (alpha * K**rho + beta * (A * L)**rho)
+
+    return epsilon_YL
