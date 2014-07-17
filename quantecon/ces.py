4fa6a91c60daf4526a5cedb486d9fc5565204fc4
David R. Pugh <drobert.pugh@gmail.com>
2014-07-17 10:30:27 -0400

    Added a function for computing the marginal product of labor.

--- quantecon/ces.py
+++ quantecon/ces.py
@@ -45,6 +45,48 @@ def marginal_product_capital(K, A, L, alpha, beta, sigma):
     return MPK
 
 
+def marginal_product_labor(K, A, L, alpha, beta, sigma):
+    """
+    Marginal product of labor for constant elasticity of substitution (CES)
+    production function with labor augmenting technology.
+
+    Arguments
+    ---------
+    K : array_like (float)
+        Capital
+    A : array_like (float)
+        Technology
+    L : array_like (float)
+        Labor
+    alpha : float
+        Importance of capital in production. Must satisfy :math:`0 < \alpha`.
+    beta : float
+        Importance of effective labor in production. Must satisfy
+        :math:`0 < \beta`.
+    sigma : float
+        Elasticity of substitution between capital and effective labor in
+        production. Must satisfy :math:`0 \le \sigma`.
+
+    Returns
+    -------
+    MPL : array_like (float)
+        Marginal product of labor.
+
+    """
+    rho = (sigma - 1) / sigma
+
+    # CES nests both Cobb-Douglas and Leontief functions
+    if abs(rho) < 1e-3:
+        MPL = (1 - beta) * K**alpha * (A * L)**(beta - 1)
+    elif sigma < 1e-3:
+        MPL = np.nan * L
+    else:
+        MPL = ((beta * (A * L)**(rho - 1) * A / (alpha * K**rho + beta * (A * L)**rho)) *
+               output(K, A, L, alpha, sigma))
+
+    return MPL
+
+
 def output(K, A, L, alpha, beta, sigma):
     """
     Constant elasticity of substitution (CES) production function with labor
@@ -54,10 +96,10 @@ def output(K, A, L, alpha, beta, sigma):
     ---------
     K : array_like (float)
         Capital
-    L : array_like (float)
-        Labor
     A : array_like (float)
         Technology
+    L : array_like (float)
+        Labor
     alpha : float
         Importance of capital in production. Must satisfy :math:`0 < \alpha`.
     beta : float
