cc44568c7e72ad39abfeb4b462ca8a2f77876ee9
David R. Pugh <drobert.pugh@gmail.com>
2014-07-17 13:25:49 -0400

    More detailed sketch of testing suite for ces.py module.

--- quantecon/tests/test_ces.py
+++ quantecon/tests/test_ces.py
@@ -1,5 +1,5 @@
 """
-Test suite for ces.py
+Testing suite for ces.py
 
 """
 import unittest
@@ -7,20 +7,109 @@ import ..ces
 
 
 class CESTestSuite(unittest.TestCase):
-
-    def test_output(self):
-        """Test function for CES output."""
-        raise NotImplementedError
+    """Base class for ces.py module tests."""
 
     def test_marginal_product_capital(self):
-        """Test function for CES marginal product of capital."""
+        """Test CES marginal product of capital."""
         raise NotImplementedError
 
     def test_marginal_product_labor(self):
-        """Test function for CES marginal product of labor."""
+        """Test CES marginal product of labor."""
+        raise NotImplementedError
+
+    def test_output(self):
+        """Test CES output."""
+        raise NotImplementedError
+
+    def test_output_elasticity_capital(self):
+        """Test CES elasticity of output with respect to capital."""
+        raise NotImplementedError
+
+    def test_output_elasticity_labor(self):
+        """Test CES elasticity of output with respect to capital."""
+        raise NotImplementedError
+
+
+class CobbDouglasCase(CESTestSuite):
+
+    sigma = 1.0
+
+    def test_marginal_product_capital(self):
+        """Test CES marginal product of capital."""
+        raise NotImplementedError
+
+    def test_marginal_product_labor(self):
+        """Test CES marginal product of labor."""
+        raise NotImplementedError
+
+    def test_output(self):
+        """Test CES output."""
+        raise NotImplementedError
+
+    def test_output_elasticity_capital(self):
+        """Test CES elasticity of output with respect to capital."""
+        raise NotImplementedError
+
+    def test_output_elasticity_labor(self):
+        """Test CES elasticity of output with respect to capital."""
+        raise NotImplementedError
+
+
+class LeontiefCase(CESTestSuite):
+
+    sigma = 1e-6
+
+    def test_marginal_product_capital(self):
+        """Test CES marginal product of capital."""
+        raise NotImplementedError
+
+    def test_marginal_product_labor(self):
+        """Test CES marginal product of labor."""
+        raise NotImplementedError
+
+    def test_output(self):
+        """Test CES output."""
+        raise NotImplementedError
+
+    def test_output_elasticity_capital(self):
+        """Test CES elasticity of output with respect to capital."""
+        raise NotImplementedError
+
+    def test_output_elasticity_labor(self):
+        """Test CES elasticity of output with respect to capital."""
+        raise NotImplementedError
+
+
+class GeneralCESCase(CESTestSuite):
+
+    def test_marginal_product_capital(self):
+        """Test CES marginal product of capital."""
+        raise NotImplementedError
+
+    def test_marginal_product_labor(self):
+        """Test CES marginal product of labor."""
+        raise NotImplementedError
+
+    def test_output(self):
+        """Test CES output."""
+        raise NotImplementedError
+
+    def test_output_elasticity_capital(self):
+        """Test CES elasticity of output with respect to capital."""
+        raise NotImplementedError
+
+    def test_output_elasticity_labor(self):
+        """Test CES elasticity of output with respect to capital."""
         raise NotImplementedError
 
 
 if __name__ == '__main__':
-    CESTest = unittest.TestLoader().loadTestsFromTestCase(CESTestSuite)
-    unittest.TextTestRunner(verbosity=2).run(suite1)
+    test_loader = unittest.TestLoader()
+
+    cobb_douglas = test_loader.loadTestsFromTestCase(CobbDouglasCase)
+    leontief = test_loader.loadTestsFromTestCase(LeontiefCase)
+    general_ces = test_loader.loadTestsFromTestCase(GeneralCESCase)
+
+    unittest.TextTestRunner(verbosity=2).run(cobb_douglas)
+    unittest.TextTestRunner(verbosity=2).run(leontief)
+    unittest.TextTestRunner(verbosity=2).run(general_ces)
