13e7b56640a7a1c45540360f33b6590597f5d437
David R. Pugh <drobert.pugh@gmail.com>
2014-07-21 17:27:51 -0400

    Started a testing example for IVP class.

--- quantecon/tests/test_ivp.py
+++ quantecon/tests/test_ivp.py
@@ -4,6 +4,8 @@ Test suite for ivp.py
 """
 import unittest
 
+from numpy import testing
+
 from ..ivp import *
 from ..solow import *
 
@@ -23,6 +25,36 @@ class IVPTestSuite(unittest.TestCase):
         raise NotImplementedError
 
 
+class SolowModelCase(IVPTestSuite):
+
+    def setUp(self):
+        # specify some parameters (g, n, s, alpha, delta, sigma)
+        params = (0.02, 0.02, 0.15, 0.33, 0.04, 1.0)
+
+        # define the initial value problem (IVP)
+        self.ivp = IVP(f=ces_k_dot, jac=ces_jacobian, args=params)
+
+    def test_integrate(self):
+        """Test ODE integration."""
+        t0 = 0.0
+        k0 = 0.5
+
+        traj = self.ivp.integrate(t0, k0, h=1.0, T=10.0, integrator='dopri5')
+
+        expected_sol = cobb_douglas_analytic_solution(traj[:0], k0, *self.ivp.args)
+        actual_sol = traj[:, 1]
+
+        testing.assert_almost_equal(expected_sol, actual_sol)
+
+
+class SpenceModelCase(IVPTestSuite):
+    pass
+
+
+class PredatorPreyModelCase(IVPTestSuite):
+    pass
+
+
 if __name__ == '__main__':
-    IVPTest = unittest.TestLoader().loadTestsFromTestCase(IVPTestSuite)
-    unittest.TextTestRunner(verbosity=2).run(suite1)
+    SolowTest = unittest.TestLoader().loadTestsFromTestCase(SolowModelCase)
+    unittest.TextTestRunner(verbosity=2).run(SolowTest)
