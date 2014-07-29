5148ab1928efd4e132265c197a73376809034f02
chase <cc7768@gmail.com>
2014-07-29 17:41:09 -0400

    py2to3: One last fix in lqramsey_ar1, import from directory not quantecon.

--- examples/lqramsey_ar1.py
+++ examples/lqramsey_ar1.py
@@ -8,26 +8,26 @@ Example 1: Govt spending is AR(1) and state is (g, 1).
 
 import numpy as np
 from numpy import array
-from quantecon import lqramsey
+import lqramsey
 
 # == Parameters == #
-beta = 1 / 1.05   
+beta = 1 / 1.05
 rho, mg = .7, .35
 A = np.identity(2)
 A[0,:] = rho, mg * (1-rho)
 C = np.zeros((2, 1))
-C[0, 0] = np.sqrt(1 - rho**2) * mg / 10 
+C[0, 0] = np.sqrt(1 - rho**2) * mg / 10
 Sg = array((1, 0)).reshape(1, 2)
 Sd = array((0, 0)).reshape(1, 2)
 Sb = array((0, 2.135)).reshape(1, 2)
 Ss = array((0, 0)).reshape(1, 2)
 
-economy = lqramsey.Economy(beta=beta, 
-        Sg=Sg, 
-        Sd=Sd, 
-        Sb=Sb, 
-        Ss=Ss, 
-        discrete=False, 
+economy = lqramsey.Economy(beta=beta,
+        Sg=Sg,
+        Sd=Sd,
+        Sb=Sb,
+        Ss=Ss,
+        discrete=False,
         proc=(A, C))
 
 T = 50
