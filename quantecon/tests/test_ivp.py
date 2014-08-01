6b9ade137078d815d292f83b4f615fbc1a3de1c6
David R. Pugh <drobert.pugh@gmail.com>
2014-08-01 21:49:45 +0100

    Added a test of the compute_residual method.

--- quantecon/tests/test_ivp.py
+++ quantecon/tests/test_ivp.py
@@ -34,6 +34,33 @@ class SolowModelCase(IVPTestSuite):
         # define the initial value problem (IVP)
         self.ivp = IVP(f=ces_k_dot, jac=ces_jacobian, args=params)
 
+    def test_compute_residual(self):
+        """Test B-spline interpolation."""
+        # analytic test case using Cobb-Douglas production technology.
+        t0 = 0.0
+        k0 = 0.5
+
+        # tighten tolerances so tests don't file due to numerical issues
+        kwargs = {'atol': 1e-12, 'rtol': 1e-9}
+
+        integrators = ['dopri5', 'dop853', 'vode', 'lsoda']
+
+        for integrator in integrators:
+
+            tmp_numeric_traj = self.ivp.integrate(t0, k0, h=1e-1, T=100.0,
+                                                  integrator=integrator,
+                                                  **kwargs)
+            T = tmp_numeric_traj[:, 0][-1]
+            tmp_grid_pts = np.linspace(t0, T, 1000)
+
+            tmp_residual = self.ivp.test_compute_residual(tmp_numeric_traj,
+                                                          tmp_grid_pts,
+                                                          k=3)
+            expected_residual = np.zeros((1000, 2))
+            actual_residual = tmp_residual
+
+            testing.assert_almost_equal(expected_residual, actual_residual)
+
     def test_integrate(self):
         """Test ODE integration."""
         # analytic test case using Cobb-Douglas production technology.
