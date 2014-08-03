831e307c2ae72a1cfd8e093acac35d441792ec72
John Stachurski <john.stachurski@gmail.com>
2014-08-03 17:01:25 +1000

    Fixed dare function...finally.  Seems all good now

--- quantecon/riccati.py
+++ quantecon/riccati.py
@@ -17,7 +17,6 @@ import warnings
 warnings.simplefilter("ignore", RuntimeWarning)
 
 
-
 def dare(A, B, Q, R, C=None, tolerance=1e-10, max_iter=150):
     """
     Solves the discrete-time algebraic Riccati equation
@@ -59,7 +58,7 @@ def dare(A, B, Q, R, C=None, tolerance=1e-10, max_iter=150):
     fail_msg = "Convergence failed after {} iterations."
 
     # == Make sure that all array_likes are np arrays, two-dimensional == #
-    A, B, Q, R = list(map(np.atleast_2d, (A, B, Q, R)))
+    A, B, Q, R = np.atleast_2d(A, B, Q, R)
     n, k = R.shape[0], Q.shape[0]
     I = np.identity(k)
     if C == None:
@@ -67,8 +66,7 @@ def dare(A, B, Q, R, C=None, tolerance=1e-10, max_iter=150):
     else:
         C = np.atleast_2d(C)
 
-    # == Choose gamma to make R_hat = R + gamma B'B invertible == #
-    R_hat = None
+    # == Choose optimal value of gamma in R_hat = R + gamma B'B == #
     current_min = np.inf
     candidates = (0.0, 0.01, 0.1, 0.25, 0.5, 1.0, 2.0, 10.0, 100.0, 10e5)
     BB = dot(B.T, B)
@@ -76,7 +74,7 @@ def dare(A, B, Q, R, C=None, tolerance=1e-10, max_iter=150):
     for gamma in candidates:
         Z = R + gamma * BB
         if np.linalg.cond(Z) > 1 / sys.float_info.epsilon:
-            pass
+            pass  # Z is ill conditioned or not invertible
         else:
             Q_tilde = - Q + dot(C.T, solve(Z, C + gamma * BTA)) + gamma * I
             G0 = dot(B, solve(Z, B.T))
@@ -84,9 +82,8 @@ def dare(A, B, Q, R, C=None, tolerance=1e-10, max_iter=150):
             H0 = gamma * dot(A.T, A0) - Q_tilde
             f1 = np.linalg.cond(Z, np.inf)
             f2 = gamma * f1
-            f3 = np.linalg.cond(I + dot(G0, H0), np.inf)
+            f3 = np.linalg.cond(I + dot(G0, H0))
             f_gamma = max(f1, f2, f3)
-            print f_gamma
             if  f_gamma < current_min:
                 best_gamma = gamma
                 current_min = f_gamma
@@ -97,18 +94,15 @@ def dare(A, B, Q, R, C=None, tolerance=1e-10, max_iter=150):
         raise ValueError(msg)
 
     gamma = best_gamma
-    print "final gamma", gamma
     R_hat = R + gamma * BB
 
+
+
+    # == Initial conditions == #
     Q_tilde = - Q + dot(C.T, solve(R_hat, C + gamma * BTA)) + gamma * I
     G0 = dot(B, solve(R_hat, B.T))
     A0 = dot(I - gamma * G0, A) - dot(B, solve(R_hat, C))
     H0 = gamma * dot(A.T, A0) - Q_tilde
-
-
-    # == Initial conditions == #
-
-    A0 = dot(I - gamma * G0, A) - dot(B, solve(R_hat, C))
     i = 1
 
     # == Main loop == #
@@ -119,8 +113,8 @@ def dare(A, B, Q, R, C=None, tolerance=1e-10, max_iter=150):
 
         else:
             A1 = dot(A0, solve(I + dot(G0, H0), A0))
-            G1 = G0 + dot(dot(A0, G0), solve(I + dot(G0, H0), A0.T))
-            H1 = H0 + dot(A0.T, solve(I + dot(G0, H0), dot(H0, A0)))
+            G1 = G0 + dot(dot(A0, G0), solve(I + dot(H0, G0), A0.T))
+            H1 = H0 + dot(A0.T, solve(I + dot(H0, G0), dot(H0, A0)))
 
             error = np.max(np.abs(H1 - H0))
             A0 = A1
@@ -129,77 +123,6 @@ def dare(A, B, Q, R, C=None, tolerance=1e-10, max_iter=150):
             i += 1
 
     return H1 + gamma * I  # Return X
-
-
-def dare_backup(A, B, R, Q, tolerance=1e-10, max_iter=150):
-    """
-    An older routine that solves the discrete-time algebraic Riccati equation
-
-        X = A'XA - A'XB(B'XB + R)^{-1}B'XA + Q
-
-    via the doubling algorithm.  Note however that this routine fails if R is
-    not invertible.  An explanation of the algorithm can be found in "Optimal
-    Filtering" by B.D.O. Anderson and J.B. Moore (Dover Publications, 2005, p.
-    159).
-
-    Parameters
-    ----------
-    A : array_like(float, ndim=2)
-        k x k array.
-    B : array_like(float, ndim=2)
-        k x n array
-    R : array_like(float, ndim=2)
-        n x n, should be symmetric and positive definite
-    Q : array_like(float, ndim=2)
-        k x k, should be symmetric and non-negative definite
-    tolerance : scalar(int), optional(default=1e-10)
-        The tolerance level for convergence
-    max_iter : scalar(float), optional(default=150)
-        The maximum number of iterations allowed
-
-    Returns
-    -------
-    X : array_like(float, ndim=2)
-        The fixed point of the Riccati equation; a  k x k array
-        representing the approximate solution
-
-    """
-    # == Set up == #
-    error = tolerance + 1
-    fail_msg = "Convergence failed after {} iterations."
-    # == Make sure that all arrays are two-dimensional == #
-    A, B, Q, R = list(map(np.atleast_2d, (A, B, Q, R)))
-    k = Q.shape[0]
-    I = np.identity(k)
-
-    # == Initial conditions == #
-    a0 = A
-    b0 = dot(B, solve(R, B.T))
-    g0 = Q
-    i = 1
-
-    # == Main loop == #
-    while error > tolerance:
-
-        if i > max_iter:
-            raise ValueError(fail_msg.format(i))
-
-        else:
-
-            a1 = dot(a0, solve(I + dot(b0, g0), a0))
-            b1 = b0 + dot(a0, solve(I + dot(b0, g0), dot(b0, a0.T)))
-            g1 = g0 + dot(dot(a0.T, g0), solve(I + dot(b0, g0), a0))
-
-            error = np.max(np.abs(g1 - g0))
-
-            a0 = a1
-            b0 = b1
-            g0 = g1
-
-            i += 1
-
-    return g1  # Return X
-
 
 
 def test_1():
@@ -220,8 +143,7 @@ def test_1():
 
     X = dare(A, B, Q, R)
     Y = np.diag((10**5, 10**3, 0))
-    print(X)
-    print(Y)
+    print np.allclose(X, Y)
 
 
 def test_1_dot_5():
@@ -241,8 +163,7 @@ def test_1_dot_5():
     X = dare(A, B, Q, R)
     Y = np.zeros((2, 2))
     Y[0, 0] = 1
-    print(X)
-    print(Y)
+    print np.allclose(X, Y)
 
 
 def test_2():
@@ -263,5 +184,6 @@ def test_2():
 
     X = dare(A, B, Q, R)
     Y = np.identity(2)
-    print(X)
-    print(Y)
+    print np.allclose(X, Y)
+
+print test_1(), test_1_dot_5(), test_2()
