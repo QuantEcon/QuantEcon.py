51ecab6dc62849e67fcc27dbb3bd894788444f4d
Spencer Lyon <spencerlyon2@gmail.com>
2014-08-04 14:28:02 -0700

    ENH: moved the lucas_tree tuple to a new LucasTree class
    Updated examples and solutions to match change

--- quantecon/models/__init__.py
+++ quantecon/models/__init__.py
@@ -6,13 +6,12 @@ objects imported here will live in the `quantecon.models` namespace
 """
 
 __all__ = ["AssetPrices", "CareerWorkerProblem", "ConsumerProblem",
-           "JvWorker", "lucas_tree", "compute_lt_price", "SearchProblem",
-           "GrowthModel"]
+           "JvWorker", "LucasTree", "SearchProblem", "GrowthModel"]
 
 from .asset_pricing import AssetPrices
 from .career import CareerWorkerProblem
 from .ifp import ConsumerProblem
 from .jv import JvWorker
-from .lucastree import lucas_tree, compute_lt_price
+from .lucastree import LucasTree
 from .odu import SearchProblem
 from .optgrowth import GrowthModel
