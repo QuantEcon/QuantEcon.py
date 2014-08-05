8a7154a25a834a830769746f9a7c6d564250d392
chase <cc7768@gmail.com>
2014-08-05 11:06:02 -0400

    TESTS: Added tests for kalman.py

--- quantecon/tests/test_kalman.py
+++ quantecon/tests/test_kalman.py
@@ -17,27 +17,71 @@ class TestKalman(unittest.TestCase):
 
     def setUp(self):
         # Initial Values
-        phi = np.array([.95, -.4, -.4])
-        theta = np.zeros(3)
-        sigma = .15
+        self.A = np.array([[.95, 0], [0., .95]])
+        self.Q = np.eye(2) * 0.5
+        self.G = np.eye(2) * .5
+        self.R = np.eye(2) * 0.2
 
+        self.kf = Kalman(A, G, Q, R)
 
-        self.lp = LinearProcess(phi, theta, sigma)
 
 
     def tearDown(self):
-        del self.lp
+        del self.kf
 
-    def test_simulate(self):
-        lp = self.lp
 
-        sim = lp.simulation(ts_length=250)
+    def test_stationarity(self):
+        A, Q, G, R = self.A, self.Q, self.G, self.R
+        kf = self.kf
 
-        self.assertTrue(sim.size==250)
+        sig_inf, kal_gain = kf.stationary_values()
 
-    def test_impulse_response(self):
-        lp = self.lp
+        mat_inv = np.linalg.inv(G.dot(sig_inf).dot(G.T) + R)
 
-        imp_resp = lp.impulse_response(impulse_length=75)
+        # Compute the kalmain gain and sigma infinity according to the
+        # recursive equations and compare
+        kal_recursion = np.dot(A, sig_inf).dot(G.T).dot(mat_inv)
+        sig_recursion = (A.dot(sig_inf).dot(A.T) -
+                            kal_gain_eq.dot(G).dot(sig_inf).dot(A.T) + Q)
 
-        self.assertTrue(imp_resp.size==75)
+        assert_allclose(kal_gain, kal_recursion)
+        assert_allclose(sig_inf, sig_recursion)
+
+
+    def test_update_using_stationary(self):
+        A, Q, G, R = self.A, self.Q, self.G, self.R
+        kf = self.kf
+
+        sig_inf, kal_gain = kf.stationary_values()
+
+        kf.set_state(np.zeros((2, 1)), sig_inf)
+
+        kf.update(np.zeros((2, 1)))
+
+        assert_allclose(kf.current_Sigma, sig_inf)
+        assert_allclose(kf.current_x_hat.squeeze(), np.zeros(2))
+
+
+    def test_update_nonstationary(self):
+        A, Q, G, R = self.A, self.Q, self.G, self.R
+        kf = self.kf
+
+        curr_x, curr_sigma = np.ones((2, 1)), np.eye(2) * .75
+        y_observed = np.ones((2, 1)) * .75
+
+        kf.set_state(curr_x, curr_sigma)
+        kf.update(y_observed)
+
+        mat_inv = np.linalg.inv(G.dot(curr_sigma).dot(G.T) + R)
+        curr_k = np.dot(A, curr_sigma).dot(G.T).dot(mat_inv)
+        new_sigma = (A.dot(curr_sigma).dot(A.T) -
+                    curr_k.dot(G).dot(curr_sigma).dot(A.T) + Q)
+
+        new_xhat = A.dot(curr_x) + curr_k.dot(y_observed - G.dot(curr_x))
+
+        assert_allclose(kf.current_Sigma, new_sigma)
+        assert_allclose(kf.current_x_hat, new_xhat)
+
+if __name__ == '__main__':
+    suite = unittest.TestLoader().loadTestsFromTestCase(TestKalman)
+    unittest.TextTestRunner(verbosity=2, stream=sys.stderr).run(suite)
