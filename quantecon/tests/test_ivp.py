2fa2723a8bf86d3ba41154197d7b210a9ba94ffd
David R. Pugh <drobert.pugh@gmail.com>
2014-08-10 23:11:24 +0100

    Updated test for IVP class.

--- quantecon/tests/test_ivp.py
+++ quantecon/tests/test_ivp.py
@@ -4,100 +4,44 @@ Test suite for ivp.py
 """
 import unittest
 
-from numpy import testing
+import numpy as np
 
-from ..ivp import *
-from ..solow import *
+from .. import ivp
+from ..models import lotka_volterra
 
 
-class SolowModelCase(unittest.TestCase):
+class LotkaVolterraTest(unittest.TestCase):
 
     def setUp(self):
-        # specify some parameters (g, n, s, alpha, delta, sigma)
-        params = (0.02, 0.02, 0.15, 0.33, 0.04, 1.0)
+        # specify some parameters (a, b, c, d)
+        params = (1.0, 0.1, 1.5, 0.75)
 
         # define the initial value problem (IVP)
-        self.ivp = IVP(f=ces_k_dot, jac=ces_jacobian, args=params)
+        self.ivp = ivp.IVP(f=lotka_volterra.f,
+                           jac=lotka_volterra.jacobian,
+                           args=params)
 
     def test_compute_residual(self):
-        """Test accuracy of IVP solvers."""
-        # analytic test case using Cobb-Douglas production technology.
-        t0 = 0.0
-        k0 = 0.5
-
-        # tighten tolerances so tests don't file due to numerical issues
-        kwargs = {'atol': 1e-12, 'rtol': 1e-9}
+        """Test integration and interpolation."""
+        # initial condition
+        t0, X0 = 0.0, np.array([10.0, 5.0])
 
         integrators = ['dopri5', 'dop853', 'vode', 'lsoda']
 
         for integrator in integrators:
 
-            tmp_numeric_traj = self.ivp.integrate(t0, k0, h=1e-1, T=100.0,
+            # tighten tolerances so tests don't file due to numerical issues
+            tmp_numeric_traj = self.ivp.integrate(t0, X0, h=1e-2, T=15.0,
                                                   integrator=integrator,
-                                                  **kwargs)
+                                                  atol=1e-14, rtol=1e-11)
             T = tmp_numeric_traj[:, 0][-1]
             tmp_grid_pts = np.linspace(t0, T, 1000)
 
+            # used highest order B-spline interpolation available
             tmp_residual = self.ivp.compute_residual(tmp_numeric_traj,
                                                      tmp_grid_pts,
-                                                     k=3)
-            expected_residual = np.zeros((1000, 2))
+                                                     k=5)
+
+            expected_residual = np.zeros((1000, 3))
             actual_residual = tmp_residual
-
-            testing.assert_almost_equal(expected_residual, actual_residual)
-
-    def test_integrate(self):
-        """Test ODE integration."""
-        # analytic test case using Cobb-Douglas production technology.
-        t0 = 0.0
-        k0 = 0.5
-
-        # tighten tolerances so tests don't file due to numerical issues
-        kwargs = {'atol': 1e-12, 'rtol': 1e-9}
-
-        integrators = ['dopri5', 'dop853', 'vode', 'lsoda']
-
-        for integrator in integrators:
-
-            tmp_numeric_traj = self.ivp.integrate(t0, k0, h=1e-1, T=100.0,
-                                                  integrator=integrator,
-                                                  **kwargs)
-
-            tmp_grid_pts = tmp_numeric_traj[:, 0]
-            tmp_analytic_traj = cobb_douglas_analytic_solution(tmp_grid_pts,
-                                                               k0,
-                                                               *self.ivp.args)
-
-            expected_sol = tmp_analytic_traj[:, 1]
-            actual_sol = tmp_numeric_traj[:, 1]
-
-            testing.assert_almost_equal(expected_sol, actual_sol)
-
-    def test_interpolate(self):
-        """Test B-spline interpolation."""
-        # analytic test case using Cobb-Douglas production technology.
-        t0 = 0.0
-        k0 = 0.5
-
-        # tighten tolerances so tests don't file due to numerical issues
-        kwargs = {'atol': 1e-12, 'rtol': 1e-9}
-
-        integrators = ['dopri5', 'dop853', 'vode', 'lsoda']
-
-        for integrator in integrators:
-
-            tmp_numeric_traj = self.ivp.integrate(t0, k0, h=1e-1, T=100.0,
-                                                  integrator=integrator,
-                                                  **kwargs)
-            T = tmp_numeric_traj[:, 0][-1]
-            tmp_grid_pts = np.linspace(t0, T, 1000)
-
-            tmp_interp_traj = self.ivp.interpolate(tmp_numeric_traj, tmp_grid_pts, k=3)
-            tmp_analytic_traj = cobb_douglas_analytic_solution(tmp_grid_pts,
-                                                               k0,
-                                                               *self.ivp.args)
-
-            expected_sol = tmp_analytic_traj[:, 1]
-            actual_sol = tmp_interp_traj[:, 1]
-
-            testing.assert_almost_equal(expected_sol, actual_sol)
+            np.testing.assert_almost_equal(expected_residual, actual_residual)
