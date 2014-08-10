fb2afbf3717846d5d3f899fa48c51685ba86acaf
John Stachurski <john.stachurski@gmail.com>
2014-08-10 20:51:17 +1000

    minor edit

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from distutils.core import setup
 setup(name='quantecon',
       packages=['quantecon', 'quantecon.models', "quantecon.tests"],
       version='0.1.4',
-      description='Code for quant-econ.net',
-      author='Thomas J. Sargent and John Stachurski',
+      description='Core package of the QuantEcon library',
+      author='Thomas J. Sargent and John Stachurski (Project coordinators)',
       author_email='john.stachurski@gmail.com',
       url='https://github.com/jstac/quant-econ',  # URL to the github repo
       download_url='https://github.com/jstac/quant-econ/tarball/0.1.4',
