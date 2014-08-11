4bb898e34d7596d8cb71bcf23b94cf63666a834b
chase <cc7768@gmail.com>
2014-08-11 13:39:53 -0400

    TESTS: Added some basic tests.

--- quantecon/tests/test_ricatti.py
+++ quantecon/tests/test_ricatti.py
@@ -8,17 +8,17 @@ Tests for ricatti.py file
 """
 import numpy as np
 from numpy.testing import assert_allclose
-from quantecon.riccati import dare
+from quantecon.matrix_eqn import solve_discrete_riccati
 
 def dare_test_golden_num_float():
-    val = dare(1.0, 1.0, 1.0, 1.0)
+    val = solve_discrete_riccati(1.0, 1.0, 1.0, 1.0)
     gold_ratio = (1 + np.sqrt(5)) / 2.
     assert_allclose(val, gold_ratio)
 
 def dare_test_golden_num_2d():
     A, B, R, Q = np.eye(2), np.eye(2), np.eye(2), np.eye(2)
     gold_diag = np.eye(2) * (1 + np.sqrt(5)) / 2.
-    val = dare(A, B, R, Q)
+    val = solve_discrete_riccati(A, B, R, Q)
     assert_allclose(val, gold_diag)
 
 def dare_test_tjm_1():
@@ -33,7 +33,7 @@ def dare_test_tjm_1():
          [0.0, 0.0, -10.0]]
     R = [[0.0, 0.0],
          [0.0, 1.0]]
-    X = dare(A, B, Q, R)
+    X = solve_discrete_riccati(A, B, Q, R)
     Y = np.diag((1e5, 1e3, 0.0))
     assert_allclose(X, Y, atol=1e-07)
 
@@ -47,7 +47,7 @@ def dare_test_tjm_2():
          [0, 0]]
     R = [[4, 2],
          [2, 1]]
-    X = dare(A, B, Q, R)
+    X = solve_discrete_riccati(A, B, Q, R)
     Y = np.zeros((2, 2))
     Y[0, 0] = 1
     assert_allclose(X, Y, atol=1e-07)
@@ -63,6 +63,6 @@ def dare_test_tjm_3():
     R = [[1, r],
          [r, r*r]]
     Q = I - np.dot(A.T, A) + np.dot(A.T, np.linalg.solve(R + I, A))
-    X = dare(A, B, Q, R)
+    X = solve_discrete_riccati(A, B, Q, R)
     Y = np.identity(2)
     assert_allclose(X, Y, atol=1e-07)
