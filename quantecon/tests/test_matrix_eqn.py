1b83af59a9d08e82cca969058490281cf748043b
chase <cc7768@gmail.com>
2014-08-14 22:22:08 -0400

    LYAP: Fixed the tests and corrected calls.

--- quantecon/tests/test_matrix_eqn.py
+++ quantecon/tests/test_matrix_eqn.py
@@ -9,7 +9,7 @@ import numpy as np
 from numpy.testing import assert_allclose
 from nose.plugins.attrib import attr
 import pandas as pd
-from quantecon import util as qeu
+from quantecon import matrix_eqn as qme
 
 
 def test_solve_discrete_lyapunov_zero():
@@ -17,10 +17,17 @@ def test_solve_discrete_lyapunov_zero():
     A = np.eye(4) * .95
     B = np.zeros((4, 4))
 
-    X = qeu.solve_discrete_lyapunov(A, B)
+    X = qme.solve_discrete_lyapunov(A, B)
 
     assert_allclose(X, np.zeros((4, 4)))
 
 
-def test_solve_discrete_lyapunov_one():
-    'Simple test where X is all ones'
+def test_solve_discrete_lyapunov_B():
+    'Simple test where X is same as B'
+    A = np.ones((2, 2)) * .5
+    B = np.array([[.5, -.5], [-.5, .5]])
+
+    X = qme.solve_discrete_lyapunov(A, B)
+
+    assert_allclose(B, X)
+
