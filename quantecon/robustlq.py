c659aab81f5efb32ec66277400ff53effea7f8fd
David R. Pugh <drobert.pugh@gmail.com>
2014-08-15 16:51:41 +0100

    Pulled and merged upstream changes

--- quantecon/robustlq.py
+++ quantecon/robustlq.py
@@ -12,7 +12,8 @@ import numpy as np
 from .lqcontrol import LQ
 from .quadsums import var_quadratic_sum
 from numpy import dot, log, sqrt, identity, hstack, vstack, trace
-from scipy.linalg import solve, inv, det, solve_discrete_lyapunov
+from scipy.linalg import solve, inv, det
+from .matrix_eqn import solve_discrete_lyapunov
 
 class RBLQ:
     r"""
---
+++ quantecon/tests/test_matrix_eqn.py
@@ -0,0 +1,33 @@
+"""
+tests for quantecon.util
+
+"""
+from __future__ import division
+from collections import Counter
+import unittest
+import numpy as np
+from numpy.testing import assert_allclose
+from nose.plugins.attrib import attr
+import pandas as pd
+from quantecon import matrix_eqn as qme
+
+
+def test_solve_discrete_lyapunov_zero():
+    'Simple test where X is all zeros'
+    A = np.eye(4) * .95
+    B = np.zeros((4, 4))
+
+    X = qme.solve_discrete_lyapunov(A, B)
+
+    assert_allclose(X, np.zeros((4, 4)))
+
+
+def test_solve_discrete_lyapunov_B():
+    'Simple test where X is same as B'
+    A = np.ones((2, 2)) * .5
+    B = np.array([[.5, -.5], [-.5, .5]])
+
+    X = qme.solve_discrete_lyapunov(A, B)
+
+    assert_allclose(B, X)
+
