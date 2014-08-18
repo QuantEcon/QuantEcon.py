f17d180da2f8150c9c381370d2ae98a8825a643c
David R. Pugh <drobert.pugh@gmail.com>
2014-08-18 12:52:08 +0100

    Now have a mechanism for caching wrapped symbolic functions.

--- quantecon/models/solow.py
+++ quantecon/models/solow.py
@@ -1,5 +1,17 @@
 r"""
+Filename: solow.py
 Author: David R. Pugh
+Date:
+
+======================
+The Solow Growth Model
+======================
+
+Assumptions
+===========
+
+Assumptions concerning the production function
+----------------------------------------------
 
 The [solow1956] model of economic growth focuses on the behavior of four
 variables: output, `Y`, capital, `K`, labor, `L`, and knowledge (or technology
@@ -13,8 +25,12 @@ output according to some production function, `F`.
 
 where `t` denotes time.
 
+The evolution of the inputs to production
+-----------------------------------------
+The initial levels of capital, labor, and technology are taken as given.
+
 References
-----------
+==========
 .. [romer2011] D. Romer. *Advanced Macroeconomics, 4th edition*, MacGraw Hill,
 2011.
 .. [solow1956] R. Solow. *A contribution to the theory of economic growth*,
@@ -55,6 +71,9 @@ class Model(ivp.IVP):
             Dictionary of model parameters.
 
         """
+        # cached values
+        self.__intensive_output = None
+
         self.output = output
         self.params = params
 
@@ -67,6 +86,19 @@ class Model(ivp.IVP):
                                         modules=[{'ImmutableMatrix': np.array}, "numpy"])
 
         super(Model, self).__init__(self._numeric_system, self._numeric_jacobian)
+
+    @property
+    def _intensive_output(self):
+        """
+        :getter: Return vectorized version of intensive aggregate production.
+        :type: function
+
+        """
+        if self.__intensive_output is None:
+            args = [k] + sp.var(self.params.keys())
+            self.__intensive_output = sp.lambdify(args, self.intensive_output,
+                                                  modules=[{'ImmutableMatrix': np.array}, "numpy"])
+        return self.__intensive_output
 
     @property
     def _symbolic_args(self):
@@ -87,9 +119,16 @@ class Model(ivp.IVP):
     @property
     def intensive_output(self):
         r"""
+        Symbolic expression for the intensive form of aggregate production.
+
+        :getter: Return the current intensive production function.
+        :type: sp.Basic
+
+        Notes
+        -----
         The assumption of constant returns to scale allows us to work the the
-        intensive form of the production function, `F`. Defining :math:`c=1/AL`
-        one can write
+        intensive form of the aggregate production function, `F`. Defining
+        :math:`c=1/AL` one can write
 
         ..math::
 
@@ -112,23 +151,28 @@ class Model(ivp.IVP):
             \lim_{k \rigtharrow 0} = \infty \\
             \lim_{k \rightarrow \infty} = 0
 
-        The Inada (1964) conditions are sufficient (but not necessary!) to
+        The [inada1964]_ conditions are sufficient (but not necessary!) to
         ensure that the time path of capital per effective worker does not
         explode.
 
-        :getter: Return the current intensive production function.
-        :type: sp.Basic
+        .. [inada1964] K. Inda. *Some structural characteristics of Turnpike Theorems*,
+        Review of Economic Studies, 31(1):43-58, 1964.
 
         """
-        return self._output.subs({'A': 1.0, 'K': k, 'L': 1.0})
+        return self.output.subs({'A': 1.0, 'K': k, 'L': 1.0})
 
     @property
     def k_dot(self):
         """
-        Symbolic equation of motion for capital per effective worker.
+        Symbolic expression defining the equation of motion for capital (per
+        worker/effective worker).
 
-        :getter: Return the current intensive production function.
+        :getter: Return the current equation of motion for capital (per worker/
+            effective worker)
         :type: sp.Basic
+
+        Notes
+        -----
 
         """
         return s * self.intensive_output - (g + n + delta) * k
@@ -136,28 +180,35 @@ class Model(ivp.IVP):
     @property
     def output(self):
         r"""
-        Solow (1956) model of economic growth focuses on the behavior of four
-        variables: output, `Y`, capital, `K`, labor, `L`, and knowledge (or
-        technology or the ``effectiveness of labor''), `A`. At each point in
-        time the economy has some amounts of capital, labor, and knowledge that
-        can be combined to produce output according to some function, `F`.
+        Symbolic expression for the aggregate production function.
+
+        :getter: Return the current aggregate production function.
+        :setter: Set a new aggregate production function
+        :type: sp.Basic
+
+        Notes
+        -----
+        At each point in time the economy has some amounts of capital, `K`,
+        labor, `L`, and knowledge (or technology), `A`, that can be combined to
+        produce output, `Y`, according to some function, `F`.
 
         .. math::
 
             Y(t) = F(K(t), A(t)L(t))
 
-        where `t` denotes time. A key assumption of the Solow model is that the
-        function `F` exhibits constant returns to scale in capital and labor.
+        where `t` denotes time. Note that `A` and `L` are assumed to enter
+        multiplicatively. Typically `A(t)L(t)` denotes "effective labor", and
+        technology that enters in this fashion is known as labor-augmenting or
+        "Harrod neutral."
+
+        A key assumption of the model is that the function `F` exhibits
+        constant returns to scale in capital and labor inputs. Specifically,
 
         .. math::
 
             F(cK(t), cA(t)L(t)) = cF(K(t), A(t)L(t)) = cY(t)
 
         for any :math:`c \ge 0`.
-
-        :getter: Return the current production function.
-        :setter: Set a new production function
-        :type: sp.Basic
 
         """
         return self._output
@@ -268,3 +319,19 @@ class Model(ivp.IVP):
             raise ValueError("Sum of g, n, and delta must be positive.")
         else:
             return params
+
+    def compute_intensive_output(self, k):
+        """
+        Parameters
+        ----------
+        k : ndarray (float)
+            Capital (per worker/effective worker)
+
+        Returns
+        -------
+        y : ndarray (float)
+            Output (per worker/effective worker)
+
+        """
+        y = self._intensive_output(k, **self.params)
+        return y
