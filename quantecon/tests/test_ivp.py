7b5498a950ee213bd5a401506b44ac35c7ecda06
David R. Pugh <drobert.pugh@gmail.com>
2014-08-27 10:57:10 -0400

    Added testing functions for ivp.py

--- quantecon/tests/test_ivp.py
+++ quantecon/tests/test_ivp.py
@@ -1,47 +1,306 @@
 """
-Test suite for ivp.py
+Tests for ivp.py
+
+@author : David R. Pugh
+@date : 2014-08-26
 
 """
-import unittest
+import nose
 
 import numpy as np
 
 from .. import ivp
-from .. models import lotka_volterra
+
+# use the Solow Model with Cobb-Douglas production as test case
+valid_params = (0.02, 0.02, 0.15, 0.33, 0.05)
+invalid_params = {'g': 0.02, 'n': 0.02, 's': 0.15, 'alpha': 0.33, 'delta': 0.05}
 
 
-class LotkaVolterraTest(unittest.TestCase):
+def solow_model(t, k, g, n, s, alpha, delta):
+    """
+    Equation of motion for capital stock (per unit effective labor).
 
-    def setUp(self):
-        # specify some parameters (a, b, c, d)
-        params = (1.0, 0.1, 1.5, 0.75)
+    Parameters
+    ----------
+    t : float
+        Time
+    k : ndarray (float, shape=(1,))
+        Capital stock (per unit of effective labor)
+    g : float
+        Growth rate of technology.
+    n : float
+        Growth rate of the labor force.
+    s : float
+        Savings rate. Must satisfy `0 < s < 1`.
+    alpha : float
+        Elasticity of output with respect to capital stock. Must satisfy
+        :math:`0 < alpha < 1`.
+    delta : float
+        Depreciation rate of physical capital. Must satisfy :math:`0 < \delta`.
 
-        # define the initial value problem (IVP)
-        self.ivp = ivp.IVP(f=lotka_volterra.f,
-                           jac=lotka_volterra.jacobian,
-                           args=params)
+    Returns
+    -------
+    k_dot : ndarray (float, shape(1,))
+        Time derivative of capital stock (per unit effective labor).
 
-    def test_compute_residual(self):
-        """Test integration and interpolation."""
-        # initial condition
-        t0, X0 = 0.0, np.array([10.0, 5.0])
+    """
+    k_dot = s * k**alpha - (g + n + delta) * k
+    return k_dot
 
-        integrators = ['dopri5', 'dop853', 'vode', 'lsoda']
 
-        for integrator in integrators:
+def solow_jacobian(t, k, g, n, s, alpha, delta):
+    """
+    Jacobian matrix for the Solow model.
 
-            # tighten tolerances so tests don't file due to numerical issues
-            tmp_numeric_traj = self.ivp.solve(t0, X0, h=1e-2, T=15.0,
-                                              integrator=integrator,
-                                              atol=1e-14, rtol=1e-11)
-            T = tmp_numeric_traj[:, 0][-1]
-            tmp_grid_pts = np.linspace(t0, T, 1000)
+    Parameters
+    ----------
+    t : float
+        Time
+    k : ndarray (float, shape=(1,))
+        Capital stock (per unit of effective labor)
+    g : float
+        Growth rate of technology.
+    n : float
+        Growth rate of the labor force.
+    s : float
+        Savings rate. Must satisfy `0 < s < 1`.
+    alpha : float
+        Elasticity of output with respect to capital stock. Must satisfy
+        :math:`0 < alpha < 1`.
+    delta : float
+        Depreciation rate of physical capital. Must satisfy :math:`0 < \delta`.
 
-            # used highest order B-spline interpolation available
-            tmp_residual = self.ivp.compute_residual(tmp_numeric_traj,
-                                                     tmp_grid_pts,
-                                                     k=5)
+    Returns
+    -------
+    jac : ndarray (float, shape(1,))
+        Time derivative of capital stock (per unit effective labor).
 
-            expected_residual = np.zeros((1000, 3))
-            actual_residual = tmp_residual
-            np.testing.assert_almost_equal(expected_residual, actual_residual)
+    """
+    jac = s * alpha * k**(alpha - 1) - (g + n + delta)
+    return jac
+
+
+def solow_steady_state(g, n, s, alpha, delta):
+    """
+    Steady-state level of capital stock (per unit effective labor).
+
+    Parameters
+    ----------
+    g : float
+        Growth rate of technology.
+    n : float
+        Growth rate of the labor force.
+    s : float
+        Savings rate. Must satisfy `0 < s < 1`.
+    alpha : float
+        Elasticity of output with respect to capital stock. Must satisfy
+        :math:`0 < alpha < 1`.
+    delta : float
+        Depreciation rate of physical capital. Must satisfy :math:`0 < \delta`.
+
+    Returns
+    -------
+    kstar : float
+        Steady state value of capital stock (per unit effective labor).
+
+    """
+    k_star = (s / (n + g + delta))**(1 / (1 - alpha))
+    return k_star
+
+
+def solow_analytic_solution(t, k0, g, n, s, alpha, delta):
+    """
+    Analytic solution for the path of capital stock (per unit effective labor).
+
+    Parameters
+    ----------
+    t : ndarray(float, shape=(1,))
+        Time
+    k : ndarray (float, shape=(1,))
+        Capital stock (per unit of effective labor)
+    g : float
+        Growth rate of technology.
+    n : float
+        Growth rate of the labor force.
+    s : float
+        Savings rate. Must satisfy `0 < s < 1`.
+    alpha : float
+        Elasticity of output with respect to capital stock. Must satisfy
+        :math:`0 < alpha < 1`.
+    delta : float
+        Depreciation rate of physical capital. Must satisfy :math:`0 < \delta`.
+
+    Returns
+    -------
+    soln : ndarray (float, shape(t.size, 2))
+        Trajectory describing the analytic solution of the model.
+
+    """
+    # lambda governs the speed of convergence
+    lmbda = (n + g + delta) * (1 - alpha)
+
+    # analytic solution for Solow model at time t
+    k_t = (((s / (n + g + delta)) * (1 - np.exp(-lmbda * t)) +
+            k0**(1 - alpha) * np.exp(-lmbda * t))**(1 / (1 - alpha)))
+
+    # combine into a (t.size, 2) array
+    analytic_traj = np.hstack((t[:, np.newaxis], k_t[:, np.newaxis]))
+
+    return analytic_traj
+
+
+# helper functions for conducting tests
+def _compute_fixed_length_solns(model, t0, k0):
+    """Returns a dictionary of fixed length solution trajectories."""
+
+    # storage containter for integration results
+    results = {}
+
+    for integrator in ['dopri5', 'dop853', 'vode', 'lsoda']:
+
+        # tighten tolerances so tests don't fail due to numerical issues
+        discrete_soln = model.solve(t0, k0, h=1e0, T=1e3,
+                                    integrator=integrator,
+                                    atol=1e-14, rtol=1e-11)
+
+        # store the result
+        results[integrator] = discrete_soln
+
+    return results
+
+
+def _termination_condition(t, k, g, n, s, alpha, delta):
+        """Terminate solver when we get close to steady state."""
+        diff = k - solow_steady_state(g, n, s, alpha, delta)
+        return diff
+
+
+def _compute_variable_length_solns(model, t0, k0, g, tol):
+    """Returns a dictionary of variable length solution trajectories."""
+
+    # storage containter for integration results
+    results = {}
+
+    for integrator in ['dopri5', 'dop853', 'vode', 'lsoda']:
+
+        # tighten tolerances so tests don't fail due to numerical issues
+        discrete_soln = model.solve(t0, k0, h=1e0, g=g, tol=tol,
+                                    integrator=integrator,
+                                    atol=1e-14, rtol=1e-11)
+
+        # store the result
+        results[integrator] = discrete_soln
+
+    return results
+
+
+# testing functions
+def test_validate_f_args():
+    """Testing validation of f_args attribute."""
+    with nose.tools.assert_raises(AttributeError):
+        model = ivp.IVP(f=solow_model,
+                        f_args=invalid_params)
+
+
+def test_validate_jac_args():
+    """Testing validation of jac_args attribute."""
+    with nose.tools.assert_raises(AttributeError):
+        model = ivp.IVP(f=solow_model,
+                        jac=solow_jacobian,
+                        jac_args=invalid_params)
+
+
+def test_solve_fixed_trajectory():
+    """Testing computation of fixed length solution trajectory."""
+    model = ivp.IVP(f=solow_model,
+                    jac=solow_jacobian,
+                    f_args=valid_params,
+                    jac_args=valid_params)
+
+    # compute some fixed length trajectories
+    t0, k0 = 0, np.array([5.0])
+    results = _compute_fixed_length_solns(model, t0, k0)
+
+    # conduct the tests
+    for integrator, numeric_solution in results.items():
+        ti = numeric_solution[:, 0]
+        analytic_solution = solow_analytic_solution(ti, k0, *valid_params)
+        np.testing.assert_allclose(numeric_solution, analytic_solution)
+
+
+def test_solve_variable_trajectory():
+    """Testing computation of variable length solution trajectory."""
+    model = ivp.IVP(f=solow_model,
+                    jac=solow_jacobian,
+                    f_args=valid_params,
+                    jac_args=valid_params)
+
+    # compute some variable length trajectories
+    t0, k0, tol = 0, np.array([5.0]), 1e-3
+    results = _compute_variable_length_solns(model, t0, k0,
+                                             g=_termination_condition, tol=tol)
+
+    # conduct the tests
+    for integrator, numeric_solution in results.items():
+        ti = numeric_solution[:, 0]
+        analytic_solution = solow_analytic_solution(ti, k0, *valid_params)
+
+        # test accuracy of solution
+        np.testing.assert_allclose(numeric_solution, analytic_solution)
+
+        # test termination condition
+        diff = numeric_solution[-1, 1] - solow_steady_state(*valid_params)
+        nose.tools.assert_less_equal(diff, tol)
+
+
+def test_interpolation():
+    """Testing parameteric B-spline interpolation methods."""
+    model = ivp.IVP(f=solow_model,
+                    jac=solow_jacobian,
+                    f_args=valid_params,
+                    jac_args=valid_params)
+
+    # compute some fixed length trajectories
+    t0, k0 = 0, np.array([5.0])
+    results = _compute_fixed_length_solns(model, t0, k0)
+
+    # conduct the tests
+    for integrator, numeric_solution in results.items():
+
+        # define an array of interpolation points
+        N, T = 1000, numeric_solution[:, 0][-1]
+        ti = np.linspace(t0, T, N)
+
+        # used highest order B-spline interpolation available
+        interp_solution = model.interpolate(numeric_solution, ti, k=3, ext=2)
+
+        analytic_solution = solow_analytic_solution(ti, k0, *valid_params)
+        np.testing.assert_allclose(interp_solution, analytic_solution)
+
+
+def test_compute_residual():
+    """Testing computation of solution residual."""
+    model = ivp.IVP(f=solow_model,
+                    jac=solow_jacobian,
+                    f_args=valid_params,
+                    jac_args=valid_params)
+
+    # compute some fixed length trajectories
+    t0, k0 = 0, np.array([5.0])
+    results = _compute_fixed_length_solns(model, t0, k0)
+
+    # conduct the tests
+    for integrator, numeric_solution in results.items():
+
+        # define an array of interpolation points
+        N, T = 1000, numeric_solution[:, 0][-1]
+        tmp_grid_pts = np.linspace(t0, T, N)
+
+        # used highest order B-spline interpolation available
+        tmp_residual = model.compute_residual(numeric_solution,
+                                              tmp_grid_pts,
+                                              k=5)
+
+        expected_residual = np.zeros((N, 2))
+        actual_residual = tmp_residual
+        np.testing.assert_almost_equal(expected_residual, actual_residual)
