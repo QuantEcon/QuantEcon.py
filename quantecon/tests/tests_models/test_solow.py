ff4c5e8dd9fce58878cca561909df562a0c74beb
David R. Pugh <drobert.pugh@gmail.com>
2014-08-31 22:45:00 -0400

    Wrote a couple of trivial tests.

--- quantecon/tests/tests_models/test_solow.py
+++ quantecon/tests/tests_models/test_solow.py
@@ -18,20 +18,26 @@ A, E, k, K, L = sym.var('A, E, k, K, L')
 # declare required model parameters
 g, n, s, alpha, delta = sym.var('g, n, s, alpha, delta')
 
+
 # use the Solow Model with Cobb-Douglas production as test case
-valid_output = K**alpha * (A * L)**(1 - alpha)
-
-
 def invalid_output(A, K, L, alpha):
     """Output must be of type sym.basic, not function."""
     return K**alpha * (A * L)**(1 - alpha)
 
-valid_params = {'g': 0.02, 'n': 0.02, 's': 0.15, 'alpha': 0.33, 'delta': 0.05}
+valid_output = K**alpha * (A * L)**(1 - alpha)
+
 invalid_params = {'g': -0.02, 'n': -0.02, 's': 0.15, 'alpha': 0.33, 'delta': 0.03}
+valid_params = {'g': 0.02, 'n': 0.02, 's': 0.15, 'alpha': 0.33, 'delta': 0.05}
 
 
 # testing functions
 def test_validate_output():
     """Testing validation of output attribute."""
     with nose.tools.assert_raises(AttributeError):
-        model = solow.model.Model(output=invalid_output, params=valid_params)
+        solow.model.Model(output=invalid_output, params=valid_params)
+
+
+def test_validate_params():
+    """Testing validation of output attribute."""
+    with nose.tools.assert_raises(AttributeError):
+        solow.model.Model(output=valid_output, params=invalid_params)
