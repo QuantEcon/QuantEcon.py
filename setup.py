5c4ad96c95ee9646f05cae98db4b6279f832a8b5
John Stachurski <john.stachurski@gmail.com>
2014-11-04 15:09:52 +1100

    Version 0.1.7

--- setup.py
+++ setup.py
@@ -29,7 +29,6 @@ setup(name='quantecon',
       description='Core package of the QuantEcon library',
       author='Thomas J. Sargent and John Stachurski (Project coordinators)',
       author_email='john.stachurski@gmail.com',
-      url='https://github.com/jstac/quant-econ',  # URL to the github repo
-      download_url='https://github.com/jstac/quant-econ/tarball/0.1.5',
+      url='https://github.com/QuantEcon/QuantEcon.py',  # URL to the github repo
       keywords=['quantitative', 'economics']
       )
