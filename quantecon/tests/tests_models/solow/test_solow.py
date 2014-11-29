33a6fd692b238d691f9201436d16e002dfb5c47b
David R. Pugh <drobert.pugh@gmail.com>
2014-11-29 16:35:55 +0000

    Moved tests into own sub-directory.

--- quantecon/tests/tests_models/solow/test_solow.py
+++ quantecon/tests/tests_models/solow/test_solow.py
@@ -130,29 +130,7 @@ def test_validate_params():
         solow.CobbDouglasModel(params=invalid_params_3)
 
 
-def test_find_steady_state():
-    """Testing computation of steady state."""
-    eps = 1e-1
-    for g in np.linspace(eps, 0.05, 4):
-        for n in np.linspace(eps, 0.05, 4):
-            for s in np.linspace(eps, 1-eps, 4):
-                for alpha in np.linspace(eps, 1-eps, 4):
-                    for delta in np.linspace(eps, 1-eps, 4):
-
-                        tmp_params = {'A0': 1.0, 'g': g, 'L0': 1.0, 'n': n,
-                                      's': s, 'alpha': alpha, 'delta': delta}
-                        tmp_mod = solow.Model(output=valid_output,
-                                              params=tmp_params)
-
-                        # use root finder to compute the steady state
-                        actual_ss = tmp_mod.steady_state
-                        expected_ss = cobb_douglas.analytic_steady_state(tmp_mod)
-
-                        # conduct the test
-                        nose.tools.assert_almost_equals(actual_ss, expected_ss)
-
-
-def test_evaluate_output_elsticity():
+def test_evaluate_output_elasticity():
     """Testing computation of elasticity of output with respect to capital."""
     eps = 1e-1
     for g in np.linspace(eps, 0.05, 4):
@@ -175,34 +153,6 @@ def test_evaluate_output_elsticity():
                         # conduct the test
                         nose.tools.assert_almost_equals(actual_elasticity,
                                                         expected_elasticity)
-
-
-def test_ivp_solve():
-    """Testing computation of solution to the initial value problem."""
-    eps = 1e-1
-    for g in np.linspace(eps, 0.05, 4):
-        for n in np.linspace(eps, 0.05, 4):
-            for s in np.linspace(eps, 1-eps, 4):
-                for alpha in np.linspace(eps, 1-eps, 4):
-                    for delta in np.linspace(eps, 1-eps, 4):
-
-                        tmp_params = {'A0': 1.0, 'g': g, 'L0': 1.0, 'n': n,
-                                      's': s, 'alpha': alpha, 'delta': delta}
-                        tmp_mod = solow.Model(output=valid_output,
-                                              params=tmp_params)
-
-                        # use root finder to compute the steady state
-                        tmp_k_star = tmp_mod.steady_state
-
-                        # solve the initial value problem
-                        t0, k0 = 0, 0.5 * tmp_k_star
-                        numeric_soln = tmp_mod.ivp.solve(t0, k0, T=100)
-
-                        tmp_ti = numeric_soln[:, 0]
-                        analytic_soln = cobb_douglas.analytic_solution(tmp_mod, tmp_ti, k0)
-
-                        # conduct the test
-                        np.testing.assert_allclose(numeric_soln, analytic_soln)
 
 
 def test_root_finders():
