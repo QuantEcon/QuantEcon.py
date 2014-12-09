b33f9d0a69bce0b5c8ef892347bc2144186509ff
David R. Pugh <drobert.pugh@gmail.com>
2014-12-09 07:52:38 +0000

    Fixed imports in __init__.py

--- quantecon/models/solow/__init__.py
+++ quantecon/models/solow/__init__.py
@@ -4,9 +4,11 @@ models directory imports
 objects imported here will live in the `quantecon.models.solow` namespace
 
 """
-__all__ = ['Model', 'CobbDouglasModel']
+__all__ = ['Model', 'CobbDouglasModel', 'CESModel']
 
 from . model import Model
 from . import model
 from . cobb_douglas import CobbDouglasModel
 from . import cobb_douglas
+from . ces import CESModel
+from . import ces
