QuantEcon/QuantEcon.py

commit 72b1d1712114f457e9f47056bd3414a7fe36b8ce

STY: made all .py files pep8 compliant

setup.py
@@ -1,14 +1,23 @@
 from distutils.core import setup
 import os
 
-#-Write a versions.py file for class attribute-#
+# Write a versions.py file for class attribute
 
 VERSION = '0.1.6'
 
+
 def write_version_py(filename=None):
-    doc = "\"\"\"\nThis is a VERSION file and should NOT be manually altered\n\"\"\""
-    doc += "\nversion = '%s'" % VERSION
-    
+    doc = '''
+    """
+    This is a VERSION file and should NOT be manually altered
+
+    """
+
+    version = "%s"
+    ''' % VERSION
+
+    return doc
+
     if not filename:
         filename = os.path.join(
             os.path.dirname(__file__), 'quantecon', 'version.py')
@@ -21,7 +30,7 @@ def write_version_py(filename=None):
 
 write_version_py()
 
-#-Setup-#
+# Setup
 
 setup(name='quantecon',
       packages=['quantecon', 'quantecon.models', "quantecon.tests"],
@@ -29,6 +38,6 @@ setup(name='quantecon',
       description='Core package of the QuantEcon library',
       author='Thomas J. Sargent and John Stachurski (Project coordinators)',
       author_email='john.stachurski@gmail.com',
-      url='https://github.com/QuantEcon/QuantEcon.py',  # URL to the github repo
+      url='https://github.com/QuantEcon/QuantEcon.py',  # URL to the repo
       keywords=['quantitative', 'economics']
       )

commit a6000364dd585dc53cda0c3e01f5440b8595a2cf

Fix issue with version function introduced in 72b1d17

quantecon/version.py
@@ -1,4 +1,4 @@
 """
 This is a VERSION file and should NOT be manually altered
 """
-version = '0.1.6'
+version = '0.1.7-2'

setup.py
@@ -6,23 +6,16 @@ import os
 
 VERSION = '0.1.7-2'
 
-
 def write_version_py(filename=None):
-    doc = '''
     """
-    This is a VERSION file and should NOT be manually altered
-
+    This constructs a version file for the project
     """
-
-    version = "%s"
-    ''' % VERSION
-
-    return doc
-
+    doc = "\"\"\"\nThis is a VERSION file and should NOT be manually altered\n\"\"\""
+    doc += "\nversion = '%s'" % VERSION
+    
     if not filename:
-        filename = os.path.join(
-            os.path.dirname(__file__), 'quantecon', 'version.py')
-
+        filename = os.path.join(os.path.dirname(__file__), 'quantecon', 'version.py')
+    
     fl = open(filename, 'w')
     try:
         fl.write(doc)