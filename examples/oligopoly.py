09e55b4fb477b2db397d98d5a9c58932e3429b52
Chase <cc7768@gmail.com>
2015-04-01 16:41:23 -0400

    OLIG: Updated oligopoly.py example with Balint and Tom's edits.

--- examples/oligopoly.py
+++ examples/oligopoly.py
@@ -1,28 +1,18 @@
 """
 Filename: oligopoly.py
-Authors: Chase Coleman
-
+Authors: Chase Coleman, Tom Sargent, Balint Szoke
 This is an example for the lecture dyn_stack.rst from the QuantEcon
 series of lectures by Tom Sargent and John Stachurski.
-
 We deal with a large monopolistic firm who faces costs:
-
 C_t = e Q_t + .5 g Q_t^2 + .5 c (Q_{t+1} - Q_t)^2
-
 where the fringe firms face:
-
 sigma_t = d q_t + .5 h q_t^2 + .5 c (q_{t+1} - q_t)^2
-
 Additionally, there is a linear inverse demand curve of the form:
-
 p_t = A_0 - A_1 (Q_t + \bar{q_t}) + \eta_t,
-
 where:
-
 .. math
     \eta_{t+1} = \rho \eta_t + C_{\varepsilon} \varepsilon_{t+1};
     \varepsilon_{t+1} \sim N(0, 1)
-
 For more details, see the lecture.
 """
 import numpy as np
@@ -36,13 +26,11 @@ def setup_matrices(params):
     """
     This function sets up the A, B, R, Q for the oligopoly problem
     described in the lecture.
-
     Parameters
     ----------
     params : Array(Float, ndim=1)
         Contains the parameters that describe the problem in the order
         [a0, a1, rho, c_eps, c, d, e, g, h, beta]
-
     Returns
     -------
     (A, B, Q, R) : Array(Float, ndim=2)
@@ -67,57 +55,65 @@ def setup_matrices(params):
                   [(a0-e)/2., 1./2, -a1 - .5*g, -a1/2, 0.],
                   [0., 0., -a1/2, 0., 0.],
                   [0., 0., 0., 0., 0.]])
+
+    Rf = np.array([[0., 0., 0., 0., 0., (a0-d)/2.],
+                  [0., 0., 0., 0., 0., 1./2.],
+                  [0., 0., 0., 0., 0., -a1/2.],
+                  [0., 0., 0., 0., 0., -a1/2.],
+                  [0., 0., 0., 0., 0., 0.],
+                  [(a0-d)/2., 1./2., -a1/2., -a1/2., 0., -h/2.]])
+
     Q = np.array([[c/2]])
 
     A = Alhsinv.dot(Arhs)
     B = Alhsinv.dot(Brhs)
 
-    return A, B, Q, R
+    return A, B, Q, R, Rf
 
 
-def find_PFd(A, B, Q, R, beta=.95):
+def find_PFd(A, B, Q, R, Rf, beta=.95):
     """
     Taking the parameters A, B, Q, R as found in the `setup_matrices`,
     we find the value function of the optimal linear regulator problem.
     This is steps 2 and 3 in the lecture notes.
-
     Parameters
     ----------
     (A, B, Q, R) : Array(Float, ndim=2)
         The matrices that describe the oligopoly problem
-
     Returns
     -------
     (P, F, d) : Array(Float, ndim=2)
         The matrix that describes the value function of the optimal
         linear regulator problem.
-
     """
 
     lq = LQ(Q, -R, A, B, beta=beta)
     P, F, d = lq.stationary_values()
 
-    return P, F, d
+    Af = np.vstack((np.hstack([A-np.dot(B,F), np.array([[0., 0., 0., 0., 0.]]).T]),np.array([[0., 0., 0., 0., 0., 1.]])))
+    Bf = np.array([[0., 0., 0., 0., 0., 1.]]).T
+
+    lqf = LQ(Q, -Rf, Af, Bf, beta=beta)
+    Pf, Ff, df = lqf.stationary_values()
+
+    return P, F, d, Pf, Ff, df
 
 
 def solve_for_opt_policy(params, eta0=0., Q0=0., q0=0.):
     """
     Taking the parameters as given, solve for the optimal decision rules
     for the firm.
-
     Parameters
     ----------
     params : Array(Float, ndim=1)
         This holds all of the model parameters in an array
-
     Returns
     -------
     out :
-
     """
     # Step 1/2: Formulate/Solve the optimal linear regulator
-    (A, B, Q, R) = setup_matrices(params)
-    (P, F, d) = find_PFd(A, B, Q, R, beta=beta)
+    (A, B, Q, R, Rf) = setup_matrices(params)
+    (P, F, d, Pf, Ff, df) = find_PFd(A, B, Q, R, Rf, beta=beta)
 
     # Step 3: Convert implementation into state variables (Find coeffs)
     P22 = P[-1, -1]
@@ -155,7 +151,7 @@ def solve_for_opt_policy(params, eta0=0., Q0=0., q0=0.):
     coeff_zt = coeffs[0, :-1]
     coeff_ztm1 = f12*(m12 - f12**(-1)*m22*f11)
 
-    return P, F, D0
+    return P, F, D0, Pf, Ff
 
 
 # Parameter values
@@ -172,9 +168,11 @@ beta = .95
 params = np.array([a0, a1, rho, c_eps, c, d, e, g, h, beta])
 
 
-P, F, D0  = solve_for_opt_policy(params)
+P, F, D0, Pf,Ff  = solve_for_opt_policy(params)
 
-print("P = "), P
-print("F = "), F
-print("D0 = "), D0
-
+# np.set_printoptions(precision=2)
+print("P = {}".format(P))
+print("F = {}".format(F))
+print("D0 = {}".format(D0))
+print("Pf = {}".format(Pf))
+print("Ff = {}".format(Ff))
