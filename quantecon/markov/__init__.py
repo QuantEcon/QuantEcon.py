e7c6ae9f8cae04106c47ca79592380ba53de3b0a
Matthew McKay <mamckay@gmail.com>
2015-08-04 11:36:53 -0400

    Migrating mc_tools.py to markov subpackage core.py file and associated tests from tests/ to markov/tests

--- quantecon/markov/__init__.py
+++ quantecon/markov/__init__.py
@@ -2,4 +2,5 @@
 Markov Chain SubPackge
 """
 
-from .random_mc import random_markov_chain, random_stochastic_matrix
+from .core import MarkovChain, mc_compute_stationary, mc_sample_path 
+from .random import random_markov_chain, random_stochastic_matrix
