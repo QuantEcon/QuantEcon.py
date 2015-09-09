e4e609f5d9a09200c55ad2ee885d7da5e0a01b75
Matthew McKay <mamckay@gmail.com>
2015-09-09 10:08:06 -0400

    Adding test for solution notebooks

--- test.py
+++ test.py
@@ -94,23 +94,39 @@ def example_tests(test_dir='examples/', log_path='example-tests.log'):
     os.chdir('../')
     return passed, failed
 
-def solutions_tests(test_dir, log_path='solutions-tests.log'):
+def solutions_tests(test_dir='solutions/', log_path='solutions-tests.log'):
     """
-    **IN-WORK**
+    Execute each Jupyter Notebook
     """
-    test_files = glob.glob(os.path.join(test_dir, '*.ipynb'))
+    os.chdir(test_dir)
+    test_files = glob.glob(os.path.join('*.ipynb'))
     test_files.sort()
     passed = []
     failed = []
     with open(log_path, 'w') as f:
-        with redirected_output(new_stdout=f, new_stderr=f):
-            for fname in test_files:
-                print("---Executing '%s'---" % fname)
-                try:
-                    subprocess.call(['runipy',fname], stdout=f, stderr=f)
-                except:
-                    print("FAIL: %s"%fname)
+            for i,fname in enumerate(test_files):
+                print("Checking notebook %s (%s/%s) ..."%(fname,i,len(test_files)))
+                with RedirectStdStreams(stdout=f, stderr=f):
+                    print("---> Executing '%s' <---" % fname)
+                    sys.stdout.flush()
+                    #-Run Program-#
+                    exit_code = subprocess.call(["runipy",fname], stdout=open(os.devnull, 'wb'), stderr=f)
+                    sys.stderr.flush()
+                    if exit_code == 0:
+                        passed.append(fname)
+                    else:
+                        failed.append(fname)
+                    print("---> END '%s' <---" % fname)
+                    print
+                    sys.stdout.flush()
+    #-Report-#
+    print "[solutions/*.py] Passed %i/%i: " %(len(passed), len(test_files))
+    print "Failed Notebooks:\n\t" + '\n\t'.join(failed)
+    print ">> See %s for details" % log_path
+    os.chdir('../')
+    return passed, failed  
 
 
 if __name__ == '__main__':
-    example_tests(*sys.argv[1:])
+    #example_tests(*sys.argv[1:])
+    solutions_tests(*sys.argv[1:])
