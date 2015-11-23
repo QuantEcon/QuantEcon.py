e90f36592ecde26452ce849fb42f2e20adb04387
Matthew McKay <mamckay@gmail.com>
2015-11-23 16:28:30 -0500

    Relocating examples/ to QuantEcon.applications repo

--- examples/calibrations/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Wed Feb 18 16:13:07 2015
-
-@author: dgevans
-"""
-
--- examples/plot_example_1.py
+++
@@ -1,7 +0,0 @@
-import matplotlib.pyplot as plt
-import numpy as np
-fig, ax = plt.subplots()
-x = np.linspace(0, 10, 200)
-y = np.sin(x)
-ax.plot(x, y, 'b-', linewidth=2)
-plt.show()
