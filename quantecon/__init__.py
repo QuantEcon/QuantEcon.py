323cbc755dc234a2faab7518b0c1bf5571fabb42
Matt McKay <mamckay@gmail.com>
2016-01-05 14:02:04 -0500

    Adding a demo notebook fetching service

--- quantecon/__init__.py
+++ quantecon/__init__.py
@@ -28,7 +28,7 @@ from .markov import mc_compute_stationary, mc_sample_path 							#Imports that S
 from .rank_nullspace import rank_est, nullspace
 from .robustlq import RBLQ
 from . import quad as quad
-from .util import searchsorted
+from .util import searchsorted, fetch_nb_dependancies
 
 #Add Version Attribute
 from .version import version as __version__
