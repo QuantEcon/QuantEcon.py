d2d492338d34fe26b9516e5f55d1ef4a3d51236a
Matt McKay <mamckay@gmail.com>
2016-01-13 10:15:30 -0500

    Adjust api to include the game_theory sub-package

--- quantecon/__init__.py
+++ quantecon/__init__.py
@@ -7,10 +7,15 @@ try:
 except:
 	raise ImportError("Cannot import numba from current anaconda distribution. Please run `conda install numba` to install the latest version.")
 
+#-Modules-#
+from . import game_theory
+
+#-Objects-#
 from .compute_fp import compute_fixed_point
 from .discrete_rv import DiscreteRV
 from .ecdf import ECDF
 from .estspec import smooth, periodogram, ar_periodogram
+# from .game_theory import <objects-here> 							#Place Holder if we wish to promote any general objects to the qe namespace.
 from .graph_tools import DiGraph
 from .gridtools import cartesian, mlinspace
 from .kalman import Kalman
@@ -22,8 +27,8 @@ from .lss import LinearStateSpace
 from .matrix_eqn import solve_discrete_lyapunov, solve_discrete_riccati
 from .quadsums import var_quadratic_sum, m_quadratic_sum
 #->Propose Delete From Top Level
-from .markov import MarkovChain, random_markov_chain, random_stochastic_matrix, gth_solve, tauchen 	 #Promote to keep current examples working
-from .markov import mc_compute_stationary, mc_sample_path 							#Imports that Should be Deprecated with markov package
+from .markov import MarkovChain, random_markov_chain, random_stochastic_matrix, gth_solve, tauchen 	 	#Promote to keep current examples working
+from .markov import mc_compute_stationary, mc_sample_path 												#Imports that Should be Deprecated with markov package
 #<-
 from .rank_nullspace import rank_est, nullspace
 from .robustlq import RBLQ
