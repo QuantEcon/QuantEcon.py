871d4261c1f76944d313e07e833b9135ad0c3812
Matt McKay <mamckay@gmail.com>
2016-03-14 17:33:03 -0400

    test removal of mock objects from conf.py in new conda environmnet

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -28,16 +28,16 @@ except ImportError:
 # ------------------------------------------------------------------- #
 # MOCK MODULES
 # ------------------------------------------------------------------- #
-on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+# on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
 
-if on_rtd:
-    class Mock(MagicMock):
-        @classmethod
-        def __getattr__(cls, name):
-            return Mock()
+# if on_rtd:
+#     class Mock(MagicMock):
+#         @classmethod
+#         def __getattr__(cls, name):
+#             return Mock()
 
-    MOCK_MODULES = ['pandas', 'statsmodels', 'numba'] 						 
-    sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
+#     MOCK_MODULES = ['pandas', 'statsmodels', 'numba'] 						 
+#     sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
