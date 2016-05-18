e73730ceb934ed9dda603d7f6e17e57b5026db02
Daisuke Oyama <oyama@e.u-tokyo.ac.jp>
2016-05-19 00:05:18 +0900

    ddp: Export sa_indices (#255)

--- docs/source/util.rst
+++ docs/source/util.rst
@@ -6,7 +6,6 @@ Utilities
 
    util/array
    util/common_messages
-   util/external
    util/notebooks
    util/random
    util/timing
---
+++ quantecon/markov/tests/test_utilities.py
@@ -0,0 +1,30 @@
+"""
+Filename: test_utilities.py
+Author: Daisuke Oyama
+
+Tests for markov/utilities.py
+
+"""
+from numpy.testing import assert_array_equal
+from quantecon.markov import sa_indices
+
+
+def test_sa_indices():
+    num_states, num_actions = 3, 4
+    s_expected = [0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 2]
+    a_expected = [0, 1, 2, 3, 0, 1, 2, 3, 0, 1, 2, 3]
+
+    s, a = sa_indices(num_states, num_actions)
+
+    for indices, indices_expected in zip([s, a], [s_expected, a_expected]):
+        assert_array_equal(indices, indices_expected)
+
+
+if __name__ == '__main__':
+    import sys
+    import nose
+
+    argv = sys.argv[:]
+    argv.append('--verbose')
+    argv.append('--nocapture')
+    nose.main(argv=argv, defaultTest=__file__)
