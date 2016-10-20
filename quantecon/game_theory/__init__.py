1843140c8cd0751acb84d7a4cfd639a6c8c2a349
Daisuke Oyama <oyama@e.u-tokyo.ac.jp>
2016-10-20 16:50:19 +0900

    Implement lemke_howson

--- quantecon/game_theory/__init__.py
+++ quantecon/game_theory/__init__.py
@@ -6,3 +6,4 @@ from .normal_form_game import Player, NormalFormGame
 from .normal_form_game import pure2mixed, best_response_2p
 from .random import random_game, covariance_game
 from .support_enumeration import support_enumeration, support_enumeration_gen
+from .lemke_howson import lemke_howson
