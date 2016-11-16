92aae778bd6e1472d1e93585fa85defe998a51e0
Daisuke Oyama <oyama@e.u-tokyo.ac.jp>
2016-11-16 17:36:55 +0900

    Implement mclennan_tourky

--- quantecon/game_theory/__init__.py
+++ quantecon/game_theory/__init__.py
@@ -7,3 +7,4 @@ from .normal_form_game import pure2mixed, best_response_2p
 from .random import random_game, covariance_game
 from .support_enumeration import support_enumeration, support_enumeration_gen
 from .lemke_howson import lemke_howson
+from .mclennan_tourky import mclennan_tourky
