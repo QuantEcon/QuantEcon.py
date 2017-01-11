4a358bc398dbe571ab66454b8524f56fa8128e79
Matt McKay <mamckay@gmail.com>
2017-01-12 10:08:15 +1100

    improve dependency fetcher with clearer parameters

--- quantecon/util/notebooks.py
+++ quantecon/util/notebooks.py
@@ -26,27 +26,25 @@ import requests
 REPO = "https://github.com/QuantEcon/QuantEcon.notebooks"
 RAW = "raw"
 BRANCH = "master"
-DEPS = "dependencies"          #Hard Coded Dependencies Folder on QuantEcon.notebooks
+FOLDER = "dependencies"          #Hard Coded Dependencies Folder on QuantEcon.notebooks
 
 
-def fetch_nb_dependencies(files, repo=REPO, raw=RAW, branch=BRANCH, deps=DEPS, overwrite=False, verbose=True):
+def fetch_nb_dependencies(files, repo=REPO, raw=RAW, branch=BRANCH, folder=FOLDER, overwrite=False, verbose=True):
     """
     Retrieve raw files from QuantEcon.notebooks or any other Github repo
     
     Parameters
     ----------
     file_list   list or dict
-                A list of files to specify a collection of filenames
+                A list of files to specify a collection of filenames	
                 A dict of dir : list(files) to specify a directory
     repo        str, optional(default=REPO)
+    raw 		str, optional(defualt=RAW)
+    			This is here in case github changes access to their raw files through web links
     branch      str, optional(default=BRANCH)
-    deps        str, optional(default=DEPS)
+    folder      str, optional(default=FOLDER)
     overwrite   bool, optional(default=False)
     verbose     bool, optional(default=True)
-
-    TODO
-    ----
-    1. Should we update this to allow people to specify their own folders on a different GitHub repo?
 
     """
 
@@ -70,7 +68,7 @@ def fetch_nb_dependencies(files, repo=REPO, raw=RAW, branch=BRANCH, deps=DEPS, o
                 if verbose: print("Overwriting file %s ..."%fl)
             if verbose: print("Fetching file: %s"%fl)
             #-Get file in OS agnostic way using requests-#
-            url = "/".join([repo,raw,branch,deps,fl])
+            url = "/".join([repo,raw,branch,folder,fl])
             r = requests.get(url)
             with open(fl, "wb") as fl:
                 fl.write(r.content)
