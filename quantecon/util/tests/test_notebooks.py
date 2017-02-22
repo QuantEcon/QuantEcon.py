c5c90c1948eb380a93c7a3c7927032f5279a7ac1
Matt McKay <mamckay@gmail.com>
2017-02-23 10:42:13 +1100

    update to remove conflict with using README.md file as a test. File already exists in travis environment

--- quantecon/util/tests/test_notebooks.py
+++ quantecon/util/tests/test_notebooks.py
@@ -11,10 +11,11 @@ from quantecon.util import fetch_nb_dependencies
 import unittest
 import os
 
-FILES = ['README.md']
+FILES = ['test_file.md']
 REPO = "https://github.com/QuantEcon/QuantEcon.py"
 RAW = "raw"
 BRANCH = "master"
+FOLDER = "quantecon/util/tests/"
 
 class TestNotebookUtils(unittest.TestCase):
 
@@ -22,16 +23,16 @@ class TestNotebookUtils(unittest.TestCase):
         """
         Run First and Test Download
         """
-        status = fetch_nb_dependencies(files=FILES, repo=REPO, raw=RAW, branch=BRANCH)
+        status = fetch_nb_dependencies(files=FILES, repo=REPO, raw=RAW, branch=BRANCH, folder=FOLDER)
         self.assertFalse(False in status)
 
     def test_fetch_nb_dependencies_overwrite(self):
         """
         Run Second and Ensure file is skipped by checking a False is found in status
         """
-        status = fetch_nb_dependencies(files=FILES, repo=REPO, raw=RAW, branch=BRANCH)  #First will succeed
-        status = fetch_nb_dependencies(files=FILES, repo=REPO, raw=RAW, branch=BRANCH)  #Second should skip
+        status = fetch_nb_dependencies(files=FILES, repo=REPO, raw=RAW, branch=BRANCH, folder=FOLDER)  #First will succeed
+        status = fetch_nb_dependencies(files=FILES, repo=REPO, raw=RAW, branch=BRANCH, folder=FOLDER)  #Second should skip
         self.assertTrue(False in status)
 
     def tearDown(self):
-        os.remove("README.md")
+        os.remove("test_file.md")
