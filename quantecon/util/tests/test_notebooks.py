7cb53bae45cdf38575aa79fc743f4889950ec866
Matt McKay <mamckay@gmail.com>
2017-02-23 10:17:01 +1100

    fix issue with tabs introduced by editor

--- quantecon/util/tests/test_notebooks.py
+++ quantecon/util/tests/test_notebooks.py
@@ -18,15 +18,15 @@ BRANCH = "master"
 class TestNotebookUtils(unittest.TestCase):
 
     def test_fetch_nb_dependencies(self):
-    	"""
-    	Run First and Test Download
-    	"""
+        """
+        Run First and Test Download
+        """
         status = fetch_nb_dependencies(files=FILES, repo=REPO, raw=RAW, branch=BRANCH)
         self.assertFalse(False in status)
 
     def test_fetch_nb_dependencies_overwrite(self):
-    	"""
-    	Run Second and Ensure file is skipped by checking a False is found in status
-    	"""
+        """
+        Run Second and Ensure file is skipped by checking a False is found in status
+        """
         status = fetch_nb_dependencies(files=FILES, repo=REPO, raw=RAW, branch=BRANCH)
         self.assertTrue(False in status)
