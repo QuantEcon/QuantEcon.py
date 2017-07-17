1382b6a6ad78bdae7a5cfe61e907ce13b108d794
Thomas Hasenzagl <thomas.hasenzagl@gmail.com>
2017-07-17 12:55:06 +0100

    rearranged tests

--- quantecon/tests/test_sylvester.py
+++ quantecon/tests/test_sylvester.py
@@ -7,9 +7,23 @@ email: thomas.hasenzagl@gmail.com
 import numpy as np
 from numpy.testing import assert_allclose
 from scipy import linalg
-from quantecon.matrix_eqn import solve_sylvester 
+from matrix_eqn import solve_sylvester 
 
-def test_sylvester(A, B, C):
+
+def test_sylvester():
+    A = np.array([[8, 1, 6], [3, 5, 7], [4, 9, 2]])
+    B = np.array([[16, 4, 1], [9, 3, 1], [4, 2, 1]])
+    C = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 0]])
+    check_sylvester(A, B, C)
+
+
+def test_cont_lyapunov():
+    B = np.array([[16, 4, 1], [9, 3, 1], [4, 2, 1]])
+    C = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 0]])
+    check_cont_lyapunov(B, C)
+
+
+def check_sylvester(A, B, C):
     """
     Tests if the solution from solve_sylvester is equal
     to the solution from scipy.linalg.solve_sylvester
@@ -27,7 +41,7 @@ def test_sylvester(A, B, C):
     assert_allclose(computed, desired)
     
 
-def test_cont_lyapunov(A, C):   
+def check_cont_lyapunov(A, C):   
     """
     Tests if the solution from solve_sylvester is equal
     to the solution from scipy.linalg.solve_lyapunov
@@ -44,12 +58,4 @@ def test_cont_lyapunov(A, C):
 
     assert_allclose(computed, desired)
     
-
-A = np.array([[8, 1, 6], [3, 5, 7], [4, 9, 2]])
-B = np.array([[16, 4, 1], [9, 3, 1], [4, 2, 1]])
-C = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 0]])
-
-test_sylvester(A, B, C)
-test_cont_lyapunov(B, C)
-  
 
