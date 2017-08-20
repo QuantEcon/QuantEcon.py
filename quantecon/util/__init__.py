c8e24efdfb4ec9ac583768d03baa2b670346c51a
QBatista <quentin.batista@mail.mcgill.ca>
2017-08-20 16:28:52 +0900

    FEAT: Add various options to timing.py

--- quantecon/util/__init__.py
+++ quantecon/util/__init__.py
@@ -5,4 +5,4 @@ API for QuantEcon Utilities
 from .array import searchsorted
 from .notebooks import fetch_nb_dependencies
 from .random import check_random_state
-from .timing import tic, tac, toc
+from .timing import tic, tac, toc, loop_timer
