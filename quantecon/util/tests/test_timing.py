45ca21e8e1244901fdae7eb3b1f667701f457014
QBatista <quentin.batista@mail.mcgill.ca>
2017-09-11 18:07:08 -0400

    TEST: Add tests to timing.py

--- quantecon/util/tests/test_timing.py
+++ quantecon/util/tests/test_timing.py
@@ -3,32 +3,86 @@ Filename: timing.py
 Authors: Pablo Winant
 Tests for timing.py
 """
+
+import time
 from numpy.testing import assert_allclose
+from nose.tools import eq_
+from quantecon.util import tic, tac, toc, loop_timer
 
 
-def test_tic_tac_toc():
+class TestTicTacToc():
+    def setUp(self):
+        self.h = 0.1 
+        self.digits = 10
+    
+    def test_timer(self):
+        tic()
 
-    from ..timing import tic, tac, toc
-    import time
+        time.sleep(self.h)
+        tm1 = float(tac())
 
-    h = 0.1
+        time.sleep(self.h)
+        tm2 = float(tac())
 
-    tic()
+        time.sleep(self.h)
+        tm3 = float(toc())
 
-    time.sleep(h)
-    el1 = tac()
+        rtol = 0.1
+        for actual, desired in zip([tm1, tm2, tm3], [self.h, self.h, self.h*3]):
+            assert_allclose(actual, desired, rtol=rtol)
 
-    time.sleep(h)
-    el2 = tac()
+    def test_digits(self):
+        tic()
+        
+        time.sleep(self.h)
+        tm1 = tac(self.digits)
 
-    time.sleep(h)
-    el3 = toc()
+        time.sleep(self.h)
+        tm2 = tac(self.digits)
 
-    rtol = 0.1
-    for actual, desired in zip([el1, el2, el3], [h, h, h*3]):
-        assert_allclose(actual, desired, rtol=rtol)
+        time.sleep(self.h)
+        tm3 = toc(self.digits)
+        
+        eq_(len(str(tm1).split(".")[1]), 10)
+        eq_(len(str(tm2).split(".")[1]), 10)
+        eq_(len(str(tm3).split(".")[1]), 10)
+        
+    def test_output(self):
+        tic()
+        
+        time.sleep(self.h)
+        tm1 = tac(self.digits, True, False)
 
+        time.sleep(self.h)
+        tm2 = tac(self.digits, True, False)
 
-if __name__ == "__main__":
+        time.sleep(self.h)
+        tm3 = toc(self.digits, True, False)
+        
+        eq_(tm1, None)
+        eq_(tm2, None)
+        eq_(tm3, None)
+        
+    def test_loop(self):
+        def test_function_one_arg(n):
+            return time.sleep(n)
 
-    test_tic_tac_toc()
+        def test_function_two_arg(n, a):
+            return time.sleep(n)
+
+        test_one_arg = loop_timer(5, test_function_one_arg, self.h, 10)
+        test_two_arg = loop_timer(5, test_function_two_arg, [self.h, 1], 10)
+        for tm in test_one_arg:
+            assert(abs(float(tm)-self.h)<0.01)
+        for tm in test_two_arg:
+            assert(abs(float(tm)-self.h)<0.01)
+
+        
+if __name__ == '__main__':
+    import sys
+    import nose
+
+    argv = sys.argv[:]
+    argv.append('--verbose')
+    argv.append('--nocapture')
+    nose.main(argv=argv, defaultTest=__file__)
