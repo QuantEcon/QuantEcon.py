5820441ff2e0a8e0c84096c0f8b61d3a9d076c8f
Daisuke Oyama <oyama@e.u-tokyo.ac.jp>
2017-09-27 09:59:13 +0900

    CLN: Modify doc style, pep8 compliance

--- quantecon/gridtools.py
+++ quantecon/gridtools.py
@@ -9,17 +9,22 @@ Implements cartesian products and regular cartesian grids.
 import numpy
 from numba import njit
 
+
 def cartesian(nodes, order='C'):
-    '''Cartesian product of a list of arrays
+    '''
+    Cartesian product of a list of arrays
 
     Parameters
     ----------
-    nodes: (list of 1d-arrays)
-    order: ('C' or 'F') order in which the product is enumerated
+    nodes : list(array_like(ndim=1))
+
+    order : str, optional(default='C')
+        ('C' or 'F') order in which the product is enumerated
 
     Returns
     -------
-    out: (2d-array) each line corresponds to one point of the product space
+    out : ndarray(ndim=2)
+        each line corresponds to one point of the product space
     '''
 
     nodes = [numpy.array(e) for e in nodes]
@@ -31,7 +36,6 @@ def cartesian(nodes, order='C'):
     l = numpy.prod(shapes)
     out = numpy.zeros((l, n), dtype=dtype)
 
-
     if order == 'C':
         repetitions = numpy.cumprod([1] + shapes[:-1])
     else:
@@ -42,23 +46,33 @@ def cartesian(nodes, order='C'):
         repetitions.reverse()
 
     for i in range(n):
-        _repeat_1d(nodes[i], repetitions[i], out[:,i])
+        _repeat_1d(nodes[i], repetitions[i], out[:, i])
 
     return out
 
+
 def mlinspace(a, b, nums, order='C'):
-    '''Constructs a regular cartesian grid
+    '''
+    Constructs a regular cartesian grid
 
     Parameters
     ----------
-    a: (1d-array) lower bounds in each dimension
-    b: (1d-array) upper bounds in each dimension
-    nums: (1d-array) number of nodes along each dimension
-    order: ('C' or 'F') order in which the product is enumerated
+    a : array_like(ndim=1)
+        lower bounds in each dimension
+
+    b : array_like(ndim=1)
+        upper bounds in each dimension
+
+    nums : array_like(ndim=1)
+        number of nodes along each dimension
+
+    order : str, optional(default='C')
+        ('C' or 'F') order in which the product is enumerated
 
     Returns
     -------
-    out: (2d-array) each line corresponds to one point of the product space
+    out : ndarray(ndim=2)
+        each line corresponds to one point of the product space
     '''
 
     a = numpy.array(a, dtype='float64')
@@ -71,14 +85,20 @@ def mlinspace(a, b, nums, order='C'):
 
 @njit
 def _repeat_1d(x, K, out):
-    '''Repeats each element of a vector many times and repeats the whole result many times
+    '''
+    Repeats each element of a vector many times and repeats the whole
+    result many times
 
     Parameters
     ----------
+    x : ndarray(ndim=1)
+        vector to be repeated
 
-    x: (1d array)       vector to be repeated
-    K: (int)            number of times each element of x is repeated (inner iterations)
-    out: (1d array)     placeholder for the result
+    K : scalar(int)
+        number of times each element of x is repeated (inner iterations)
+
+    out : ndarray(ndim=1)
+        placeholder for the result
 
     Returns
     -------
@@ -86,8 +106,8 @@ def _repeat_1d(x, K, out):
     '''
 
     N = x.shape[0]
-    L = out.shape[0] // (K*N) # number of outer iterations
-    # K                       # number of inner iterations
+    L = out.shape[0] // (K*N)  # number of outer iterations
+    # K                        # number of inner iterations
 
     # the result out should enumerate in C-order the elements
     # of a 3-dimensional array T of dimensions (K,N,L)
