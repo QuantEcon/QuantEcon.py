96f88809fdc8dd12e127167ddb4e25ea92e8bdf6
Daisuke Oyama <oyama@e.u-tokyo.ac.jp>
2017-12-28 23:02:49 +0900

    Add random_tournament_graph

--- quantecon/__init__.py
+++ quantecon/__init__.py
@@ -19,7 +19,7 @@ from .discrete_rv import DiscreteRV
 from .ecdf import ECDF
 from .estspec import smooth, periodogram, ar_periodogram
 # from .game_theory import <objects-here> 							#Place Holder if we wish to promote any general objects to the qe namespace.
-from .graph_tools import DiGraph
+from .graph_tools import DiGraph, random_tournament_graph
 from .gridtools import cartesian, mlinspace, simplex_grid, simplex_index
 from .kalman import Kalman
 from .lae import LAE
