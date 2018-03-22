9517eed193fe34dbb1bc0015deee9c084c05cb87
Shunsuke Hori <hori0518@gmail.com>
2018-03-22 15:52:55 -0700

    delete file and author name and add random walk case

--- quantecon/filter.py
+++ quantecon/filter.py
@@ -1,13 +1,11 @@
 """
-Filename: filter.py
-Authors: Shunsuke Hori
 
 function for filtering
 
 """
 import numpy as np
 
-def hamilton_filter(data, h, p):
+def hamilton_filter(data, h, *args):
     r"""
     This function applies "Hamilton filter" to the data
     
@@ -20,9 +18,10 @@ def hamilton_filter(data, h, p):
         Time horizon that we are likely to predict incorrectly.
         Original paper recommends 2 for annual data, 8 for quarterly data,
         24 for monthly data.
-    p : integer
-        Number of lags in regression. 
+    *args : integer
+        If supplied, it is p in the paper. Number of lags in regression. 
         Must be greater than h.
+        If not supplied, random walk process is assumed.
         
     Note: For seasonal data, it's desirable for p and h to be integer multiples
           of the number of obsevations in a year.
@@ -38,17 +37,26 @@ def hamilton_filter(data, h, p):
     y = np.asarray(data, float)
     # sample size
     T = len(y)
-    # construct X matrix of lags
-    X = np.ones((T-p-h+1, p+1))
-    for j in range(1, p+1):
-        X[:, j] = y[p-j:T-h-j+1:1]
 
-    # do OLS regression
-    b = np.linalg.solve(X.transpose()@X, X.transpose()@y[p+h-1:T])
-    # trend component (`nan` for the first p+h-1 period)
-    trend = np.append(np.zeros(p+h-1)+np.nan, X@b)
-    # cyclical component
-    cycle = data - trend
+    if len(args) == 1: # if p is supplied
+        p = args[0]
+        # construct X matrix of lags
+        X = np.ones((T-p-h+1, p+1))
+        for j in range(1, p+1):
+            X[:, j] = y[p-j:T-h-j+1:1]
 
+        # do OLS regression
+        b = np.linalg.solve(X.transpose()@X, X.transpose()@y[p+h-1:T])
+        # trend component (`nan` for the first p+h-1 period)
+        trend = np.append(np.zeros(p+h-1)+np.nan, X@b)
+        # cyclical component
+        cycle = y - trend
+
+    elif len(args) == 0: # if p is not supplied (random walk)
+        cycle = np.append(np.zeros(h)+np.nan, y[h:T] - y[0:T-h])
+        trend = y - cycle
+    
     return cycle, trend
+
+
 
