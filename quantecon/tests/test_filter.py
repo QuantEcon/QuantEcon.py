60538a46cd48830a51125edaf49c377a128707d3
Shunsuke Hori <hori0518@gmail.com>
2018-03-22 17:33:44 -0700

    fix test

--- quantecon/tests/test_filter.py
+++ quantecon/tests/test_filter.py
@@ -17,13 +17,13 @@ def test_hamilton_filter():
     # read data
     data_dir = get_data_dir()
     data = pd.read_csv(os.path.join(data_dir, "employment.csv"),
-                       names = ['year', 'employment', 'matlab_cycle', 'matlab_cycle_rw'])
+                       names = ['year', 'employment', 'matlab_cyc', 'matlab_cycle_rw'])
 
     data['hamilton_cyc'], data['hamilton_trend'] =  hamilton_filter(100*np.log(data['employment']), 8, 4)
     data['hamilton_cyc_rw'], data['hamilton_trend_rw'] = hamilton_filter(100*np.log(data['employment']), 8)
-    assert_allclose(data['matlab_cycle'], data['hamilton_cycle'],
+    assert_allclose(data['matlab_cyc'], data['hamilton_cyc'],
                     rtol = 1e-07, atol = 1e-07)
-    assert_allclose(data['matlab_cycle_rw'], data['hamilton_cycle_rw'])
+    assert_allclose(data['matlab_cyc_rw'], data['hamilton_cyc_rw'])
 
 
 if __name__ == '__main__':
