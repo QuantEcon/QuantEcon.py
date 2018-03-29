c9a767017b7a1cd84ec8345891c2efe2f5be674a
Shunsuke-Hori <hori0518@gmail.com>
2018-03-28 21:57:53 -0700

    Add hamilton filter (#405)
    * initial commit for hamilton filter
    
    * return array instead of dataframe
    
    * increase precision of matlab output
    
    * delete file and author name and add random walk case
    
    * update test
    
    * fix test
    
    * fix test
    
    * make tolerance larger
    
    * change data location
    
    * update matlab output

---
+++ quantecon/tests/test_filter.py
@@ -0,0 +1,31 @@
+"""
+Tests for filter.py.
+Using the data of original paper.
+
+"""
+
+import sys
+import os
+import unittest
+import pandas as pd
+import numpy as np
+from numpy.testing import assert_allclose
+from quantecon.filter import hamilton_filter
+from quantecon.tests.util import get_data_dir
+
+def test_hamilton_filter():
+    # read data
+    data_dir = get_data_dir()
+    data_dir = 'data'
+    data = pd.read_csv(os.path.join(data_dir, "employment.csv"),
+                       names = ['year', 'employment', 'matlab_cyc', 'matlab_cyc_rw'])
+
+    data['hamilton_cyc'], data['hamilton_trend'] =  hamilton_filter(100*np.log(data['employment']), 8, 4)
+    data['hamilton_cyc_rw'], data['hamilton_trend_rw'] = hamilton_filter(100*np.log(data['employment']), 8)
+    assert_allclose(data['matlab_cyc'], data['hamilton_cyc'],
+                    rtol = 1e-07, atol = 1e-07)
+    assert_allclose(data['matlab_cyc_rw'], data['hamilton_cyc_rw'])
+
+
+if __name__ == '__main__':
+    test_hamilton_filter()
