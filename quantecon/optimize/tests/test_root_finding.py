a4ba56f4aaa812947b21f8917be20a20645a9b8f
Supavit Dumrongprechachan <spvdchachan@users.noreply.github.com>
2018-07-25 14:18:32 +1000

    Add bisection and brent's method for root finding
    I added two jitted robust root finding methods: Bisection and Brentq
    which are based on Scipy's version which is written in C. They both
    follow the previous jitted root finding procedures by returning a
    namedtuple with relevant information.
    
    I also added a basic test for each method.

--- quantecon/optimize/tests/test_root_finding.py
+++ quantecon/optimize/tests/test_root_finding.py
@@ -2,7 +2,8 @@ import numpy as np
 from numpy.testing import assert_almost_equal, assert_allclose
 from numba import njit
 
-from quantecon.optimize import newton, newton_halley, newton_secant
+from quantecon.optimize import *
+
 
 @njit
 def func(x):
@@ -19,12 +20,14 @@ def func_prime(x):
     """
     return (3*x**2)
 
+
 @njit
 def func_prime2(x):
     """
     Second order derivative for func.
     """
     return 6*x
+
 
 @njit
 def func_two(x):
@@ -40,6 +43,7 @@ def func_two_prime(x):
     Derivative for func_two.
     """
     return 4*np.cos(4*(x - 1/4)) + 20*x**19 + 1
+
 
 @njit
 def func_two_prime2(x):
@@ -67,8 +71,8 @@ def test_newton_basic_two():
     true_fval = 1.0
     fval = newton(func, 5, func_prime)
     assert_allclose(true_fval, fval.root, rtol=1e-5, atol=0)
-    
-    
+
+
 def test_newton_hard():
     """
     Harder test for convergence.
@@ -76,6 +80,7 @@ def test_newton_hard():
     true_fval = 0.408
     fval = newton(func_two, 0.4, func_two_prime)
     assert_allclose(true_fval, fval.root, rtol=1e-5, atol=0.01)
+
     
 def test_halley_basic():
     """
@@ -85,6 +90,7 @@ def test_halley_basic():
     fval = newton_halley(func, 5, func_prime, func_prime2)
     assert_almost_equal(true_fval, fval.root, decimal=4)
 
+
 def test_halley_hard():
     """
     Harder test for halley method
@@ -92,6 +98,7 @@ def test_halley_hard():
     true_fval = 0.408
     fval = newton_halley(func_two, 0.4, func_two_prime, func_two_prime2)
     assert_allclose(true_fval, fval.root, rtol=1e-5, atol=0.01)
+
 
 def test_secant_basic():
     """
@@ -111,7 +118,24 @@ def test_secant_hard():
     assert_allclose(true_fval, fval.root, rtol=1e-5, atol=0.01)
 
 
+def run_check(method, name):
+    a = -1
+    b = np.sqrt(3)
+    true_fval = 0.408
+    r = method(func_two, a, b)
+    assert_allclose(true_fval, r.root, atol=0.01, rtol=1e-5,
+                    err_msg='method %s' % name)
+
+
+def test_bisect_basic():
+    run_check(bisect, 'bisect')
+
+
+def test_brentq_basic():
+    run_check(brentq, 'brentq')
+
 # executing testcases.
+
 
 if __name__ == '__main__':
     import sys
