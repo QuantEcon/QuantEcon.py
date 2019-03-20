2ed25e8e30edeb8e1ed64db92de580d1ec7421d0
Yuya Furusawa <fullyou0798@gmail.com>
2019-03-21 02:16:55 +0900

    Fix fictplay.py and add test_fictplay.py

--- quantecon/game_theory/fictplay.py
+++ quantecon/game_theory/fictplay.py
@@ -1,97 +1,185 @@
 import numpy as np
+from .normal_form_game import NormalFormGame, pure2mixed
+from ..util import check_random_state
+from .random import random_pure_actions
 
-class FictitiousPlay(object):
 
-	def __init__(self, data, gain=None):
-		if isinstance(data, NormalFormGame):
-			self.g = data
-		else:  # data must be array_like
-			payoffs = np.asarray(data)
-			self.g = NormalFormGame(payoffs)
+class FictitiousPlay:
+    """
+    Class representing the fictitious play model.
 
-		self.N = self.g.N 
-		self.players = self.g.players
-		self.nums_actions = self.g.nums_actions
-		self.tie_breaking = 'smallest'
+    Parameters
+    ----------
+    data : NormalFormGame or array-like
+        The game played in fictitious play model
 
-		if gain is None:
-			self.step_size = lambda t: 1 / (t+1) # decreasing gain
-		else:
-			self.step_size = lambda t: gain # constant gain
+    gain : scalar(float), optional(default=None)
+        The gain of fictitous play model. If gain is None, the model becomes
+        decreasing gain model. If gain is scalar, the model becomes constant
+        constant gain model.
 
-	def _play(self, actions, t):
-		brs = np.zeros(self.N, dtype=int)
-		for i, player in enumerate(self.players):
-			index = [j for j in range(i+1, self.N)]
-			index.extend([j for j in range(i)])
-			opponent_actions = np.asarray([actions[i] for i in index])
-			brs[i] = player.best_response(opponent_actions
-					if self.N > 2 else opponent_actions[0],
-					tie_breaking=self.tie_breaking)
+    Attributes
+    ----------
+    g : NomalFormGame
+        The game. See Parameters.
 
-		for i in range(self.N):
-			actions[i][:] *= 1 - self.step_size(t+1)
-			actions[i][brs[i]] += self.step_size(t+1)
+    N : scalar(int)
+        The number of players in the model.
 
-		return actions
+    players : Player
+        Player instance in the model.
 
-	def play(self, init_actions=None, num_reps=1, t_init=0):
-		if init_actions is None:
-			init_actions = random_pure_actions(self.nums_actions)
-		actions = [i for i in init_actions]
-		for i in range(self.N):
-			actions[i] = pure2mixed(self.nums_actions[i], init_actions[i])
-		for t in range(num_reps):
-			actions = self._play(actions, t+t_init)
-		return actions
+    nums_actions : tuple(int)
+        Tuple of the number of actions, one for each player.
+    """
 
-	def time_series(self, ts_length, init_actions=None, t_init=0):
-		if init_actions is None:
-			init_actions = random_pure_actions(self.nums_actions)
-		out = [np.empty((ts_length, self.nums_actions[i]))
-			for i in range(self.N)]
-		actions = [np.empty(self.nums_actions[i]) for i in range(self.N)]
-		for i in range(self.N):
-			actions[i] = pure2mixed(self.nums_actions[i], init_actions[i])[:]
-		for t in range(ts_length):
-			for i in range(self.N):
-				out[i][t,:] = actions[i][:] 
-			actions = self._play(actions, t+t_init)
-		return out
+    def __init__(self, data, gain=None):
+        if isinstance(data, NormalFormGame):
+            self.g = data
+        else:  # data must be array_like
+            payoffs = np.asarray(data)
+            self.g = NormalFormGame(payoffs)
+
+        self.N = self.g.N
+        self.players = self.g.players
+        self.nums_actions = self.g.nums_actions
+        self.tie_breaking = 'smallest'
+
+        if gain is None:
+            self.step_size = lambda t: 1 / (t+1)  # decreasing gain
+        else:
+            self.step_size = lambda t: gain  # constant gain
+
+    def _play(self, actions, t, random_state=None):
+        brs = np.zeros(self.N, dtype=int)
+        for i, player in enumerate(self.players):
+            index = [j for j in range(i+1, self.N)]
+            index.extend([j for j in range(i)])
+            opponent_actions = np.asarray([actions[i] for i in index])
+            brs[i] = player.best_response(
+                opponent_actions if self.N > 2 else opponent_actions[0],
+                tie_breaking=self.tie_breaking)
+
+        for i in range(self.N):
+            actions[i][:] *= 1 - self.step_size(t+1)
+            actions[i][brs[i]] += self.step_size(t+1)
+
+        return actions
+
+    def play(self, init_actions=None, num_reps=1, t_init=0, random_state=None):
+        """
+        Return a new action profile which is updated by playing the game
+        `num_reps` times.
+
+        Parameters
+        ----------
+        init_actions : tuple(int), optional(default=None)
+            The action profile in the first period. If None, selected randomly.
+
+        num_reps : scalar(int), optional(default=1)
+            The number of iterations.
+
+        t_init : scalar(int), optional(default=0)
+            The period the game starts.
+
+        Returns
+        -------
+        tuple(int)
+            The action profile after iteration.
+        """
+        random_state = check_random_state(random_state)
+        if init_actions is None:
+            init_actions = random_pure_actions(self.nums_actions, random_state)
+        actions = [i for i in init_actions]
+        for i in range(self.N):
+            actions[i] = pure2mixed(self.nums_actions[i], init_actions[i])
+        for t in range(num_reps):
+            actions = self._play(actions, t+t_init, random_state)
+        return actions
+
+    def time_series(self, ts_length, init_actions=None, t_init=0,
+                    random_state=None):
+        """
+        Return the array representing time series of normalized action history.
+
+        Parameters
+        ----------
+        ts_length : scalar(int)
+            The number of iterations.
+
+        init_actions : tuple(int), optional(default=None)
+            The action profile in the first period. If None, selected randomly.
+
+        t_init : scalar(int), optional(default=0)
+            The period the game starts.
+
+        Returns
+        -------
+        Array
+            The array representing time series of normalized action history.
+        """
+        random_state = check_random_state(random_state)
+        if init_actions is None:
+            init_actions = random_pure_actions(self.nums_actions, random_state)
+        out = [np.empty((ts_length, self.nums_actions[i]))
+               for i in range(self.N)]
+        actions = [np.empty(self.nums_actions[i]) for i in range(self.N)]
+        for i in range(self.N):
+            actions[i] = pure2mixed(self.nums_actions[i], init_actions[i])[:]
+        for t in range(ts_length):
+            for i in range(self.N):
+                out[i][t, :] = actions[i][:]
+            actions = self._play(actions, t+t_init, random_state)
+        return out
+
 
 class StochasticFictitiousPlay(FictitiousPlay):
+    """
+    Class representing the stoochastic fictitous play model.
+    Subclass of FictitiousPlay.
 
-	def __init__(self, data, gain=None, distribution='extreme'):
-		FictitiousPlay.__init__(self, data, gain)
+    Parameters
+    ----------
+    data : NormalFormGame or array-like
+        The game played in the stochastic fictitious play model.
 
-		if distribution == 'extreme':
-			loc = -np.euler_gamma * np.sqrt(6) / np.pi
-			scale = np.sqrt(6) / np.pi
-			self.payoff_perturbation_dist = \
-				lambda size: np.random.gumbel(loc=loc, scale=scale, size=size)
-		elif distribution == 'normal':  # standard normal distribution
-			self.payoff_perturbation_dist = \
-				lambda size: np.random.standard_normal(size=size)
-		else:
-			raise ValueError("`distribution` must be 'extreme' or 'normal'")
+    distribution : scipy.stats object
+        statistical distribution from scipy.stats
 
-		self.tie_breaking = 'smallest'
+    gain : scalar(int), optional(default=None)
+        The gain of fictitous play model. If gain is None, the model becomes
+        decreasing gain model. If gain is scalar, the model becomes constant
+        constant gain model.
 
-	def _play(self, actions, t):
-		brs = np.zeros(self.N, dtype=int)
-		for i, player in enumerate(self.players):
-			index = [j for j in range(i+1, self.N)]
-			index.extend([j for j in range(i)])
-			opponent_actions = np.asarray([actions[i] for i in index])
-			payoff_perturbation = \
-				self.payoff_perturbation_dist(size=self.nums_actions[i])
-			brs[i] = player.best_response(opponent_actions
-				if self.N > 2 else opponent_actions[0],
-				tie_breaking=self.tie_breaking,
-				payoff_perturbation=payoff_perturbation)
+    Attributes
+    ----------
+    See attributes of FictitousPlay.
+    """
 
-		for i in range(self.N):
-			actions[i][:] *= 1 - self.step_size(t+1)
-			actions[i][brs[i]] += self.step_size(t+1)
+    def __init__(self, data, distribution, gain=None):
+        FictitiousPlay.__init__(self, data, gain)
+        self.payoff_perturbation_dist = \
+            lambda size: distribution.rvs(size=size, random_state=random_state)
 
-		return actions
+        self.tie_breaking = 'smallest'
+
+    def _play(self, actions, t, random_state=None):
+        random_state = check_random_state(random_state)
+        brs = np.zeros(self.N, dtype=int)
+        for i, player in enumerate(self.players):
+            index = [j for j in range(i+1, self.N)]
+            index.extend([j for j in range(i)])
+            opponent_actions = np.asarray([actions[i] for i in index])
+            payoff_perturbation = \
+                self.payoff_perturbation_dist(size=self.nums_actions[i],
+                                              random_state=random_state)
+            brs[i] = player.best_response(
+                opponent_actions if self.N > 2 else opponent_actions[0],
+                tie_breaking=self.tie_breaking,
+                payoff_perturbation=payoff_perturbation)
+
+        for i in range(self.N):
+            actions[i][:] *= 1 - self.step_size(t+1)
+            actions[i][brs[i]] += self.step_size(t+1)
+
+        return actions
---
+++ quantecon/game_theory/tests/test_fictplay.py
@@ -0,0 +1,119 @@
+"""
+Filename: test_fictplay.py
+Tests for fictplay.py
+"""
+
+import numpy as np
+from numpy.testing import assert_array_almost_equal
+from scipy.stats import norm
+
+from quantecon.game_theory import FictitiousPlay, StochasticFictitiousPlay
+
+
+class Test_FictitiousPlay_DecreaingGain:
+
+    def setUp(self):
+        '''Setup a FictitiousPlay instance'''
+        # symmetric 2x2 coordination game
+        matching_pennies = [[( 1, -1), (-1,  1)],
+                            [(-1,  1), ( 1, -1)]]
+        self.fp = FictitiousPlay(matching_pennies)
+
+    def test_play(self):
+        x = [np.asarray([1, 0]), np.asarray([0.5, 0.5])]
+        assert_array_almost_equal(self.fp.play(init_actions=(0, 0)), x)
+
+    def test_time_series(self):
+        x = self.fp.time_series(ts_length=3, init_actions=(0, 0))
+        assert_array_almost_equal(x[0],
+            [[1, 0],
+             [1, 0],
+             [1, 0]])
+        assert_array_almost_equal(x[1],
+            [[1,0],
+             [1/2, 1/2],
+             [1/3, 2/3]])
+
+
+class Test_FictitiousPlay_ConstantGain:
+
+    def setUp(self):
+        matching_pennies = [[( 1, -1), (-1,  1)],
+                            [(-1,  1), ( 1, -1)]]
+        self.fp = FictitiousPlay(matching_pennies, gain=0.1)
+
+    def test_play(self):
+        x = [np.asarray([1, 0]), np.asarray([0.9, 0.1])]
+        assert_array_almost_equal(self.fp.play(init_actions=(0, 0)), x)
+
+    def test_time_series(self):
+        x = self.fp.time_series(ts_length=3, init_actions=(0, 0))
+        assert_array_almost_equal(x[0],
+            [[1, 0],
+             [1, 0],
+             [1, 0]]
+            )
+        assert_array_almost_equal(x[1],
+            [[1, 0],
+             [0.9, 0.1],
+             [0.81, 0.19]])
+
+class Test_StochasticFictitiosuPlay_DecreaingGain:
+
+    def setUp(self):
+        matching_pennies = [[( 1, -1), (-1,  1)],
+                            [(-1,  1), ( 1, -1)]]
+        distribution= norm()
+        self.fp = StochasticFictitiousPlay(matching_pennies,
+                                           distribution=distribution)
+
+    def test_play(self):
+        seed = 1234
+        x = [self.fp.play(init_actions=(0, 0),
+                          random_state=np.random.RandomState(seed))
+             for i in range(2)]
+        assert_array_almost_equal(x[0], x[1])
+
+    def test_time_series(self):
+        seed = 1234
+        x = [self.fp.time_series(ts_length=3, init_actions=(0, 0),
+                                 random_state=np.random.RandomState(seed))
+             for i in range(2)]
+        assert_array_almost_equal(x[0][0], x[1][0])
+        assert_array_almost_equal(x[0][1], x[1][1])
+        
+
+class Test_StochasticFictitiosuPlay_ConstantGain:
+
+    def setUp(self):
+        matching_pennies = [[( 1, -1), (-1,  1)],
+                            [(-1,  1), ( 1, -1)]]
+        distribution = norm()
+        self.fp = StochasticFictitiousPlay(matching_pennies, gain=0.1,
+                                 distribution=distribution)
+
+    def test_play(self):
+        seed = 1234
+        x = [self.fp.play(init_actions=(0, 0),
+                          random_state=np.random.RandomState(seed))
+             for i in range(2)]
+        assert_array_almost_equal(x[0], x[1])
+
+    def test_time_series(self):
+        seed = 1234
+        x = [self.fp.time_series(ts_length=3, init_actions=(0, 0),
+                                 random_state=np.random.RandomState(seed))
+             for i in range(2)]
+        assert_array_almost_equal(x[0][0], x[1][0])
+        assert_array_almost_equal(x[0][1], x[1][1])
+
+# Invalid inputs #
+
+if __name__ == '__main__':
+    import sys
+    import nose
+
+    argv = sys.argv[:]
+    argv.append('--verbose')
+    argv.append('--nocapture')
+    nose.main(argv=argv, defaultTest=__file__)
