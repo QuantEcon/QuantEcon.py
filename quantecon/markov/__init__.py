8295961f7347d5e9badfbb2c9ea20703e8b1049c
Smit-create <smitlunagariya.mat18@itbhu.ac.in>
2022-11-03 19:49:41 +0530

    Add discrete_var in approximation

--- quantecon/markov/__init__.py
+++ quantecon/markov/__init__.py
@@ -8,6 +8,6 @@ from .core import mc_compute_stationary, mc_sample_path 			#-Future Deprecation-
 from .gth_solve import gth_solve
 from .random import random_markov_chain, random_stochastic_matrix, \
     random_discrete_dp
-from .approximation import tauchen, rouwenhorst
+from .approximation import tauchen, rouwenhorst, discrete_var
 from .ddp import DiscreteDP, backward_induction
 from .utilities import sa_indices
