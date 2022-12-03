d54aa817b9cdeb88bf580cd2e207a55db229d577
Daisuke Oyama <oyama@e.u-tokyo.ac.jp>
2022-12-03 21:29:28 +0900

    Rewrite to accept path of state values

--- quantecon/markov/estimate.py
+++ quantecon/markov/estimate.py
@@ -2,71 +2,53 @@ import numpy as np
 from numba import njit
 from .core import MarkovChain
 
-def estimate_mc_discrete(index_series, state_values=None):
-    r"""
-    Estimates the Markov chain associated with discrete-valued
-    time series data :math:`(X_0, ..., X_{k-1})` that is assumed to be
-    Markovian.  The estimation is by maximum likelihood. Transition
-    probabilities are returned as a matrix :math:`P` where :math:`P[i, j] =
-    N_{ij} / N_i`.  Here :math:`N_{ij}` is :math:`\sum_{t=0}^{k-1} 1\{X_t=i,\;
-    X_{t+1}=j\}`, the number of transitions from i to j, while :math:`N_i` is
-    total number of visits to i.
 
+def estimate_mc_discrete(X):
+    r"""
+    Estimate the Markov chain associated with a time series :math:`X =
+    (X_0, \ldots, X_{T-1})` assuming that the state space is the finite
+    set :math:`\{X_0, \ldots, X_{T-1}\}` (duplicates removed). The
+    estimation is by maximum likelihood. The estimated transition
+    probabilities are given by the matrix :math:`P` such that
+    :math:`P[i, j] = N_{ij} / N_i`, where :math:`N_{ij} =
+    \sum_{t=0}^{T-1} 1_{\{X_t=s_i, X_{t+1}=s_j\}}`, the number of
+    transitions from state :math:`s_i` to state :math:`s_j`, while
+    :math:`N_i` is the total number of visits to :math:`s_i`. The result
+    is returned as a `MarkovChain` instance.
 
     Parameters
     ----------
-
-    index_series : array_like (int, ndim=1)
-        A time series in index form, from which the transition matrix
-        will be estimated.  A sequence (i_0, i_1, ..., i_{k-1}) where
-        element i_t indicates that the Markov chain is in state
-        state_values[i_t] at time t.
-
-    state_values : array_like (default=None, ndim=1)
-        A flat array_like of length n containing the values associated with the
-        states.   If state_values is None, then state_values is set to the
-        integer sequence (0, ..., n-1), where n = max(index_series).
+    X : array_like
+        A time series of state values, from which the transition matrix
+        will be estimated.
 
     Returns
     -------
-
     mc : MarkovChain
-        A MarkovChain object where mc.P is a stochastic matrix estimated from
-        the index_series data and mc.state_values is the states of the Markov
-        chain.
+        A MarkovChain instance where `mc.P` is a stochastic matrix
+        estimated from the data `X` and `mc.state_values` is an array of
+        values that appear in `X` (sorted in ascending order).
 
     """
+    X = np.asarray(X)
+    axis = 0 if X.ndim > 1 else None
+    state_values, indices = np.unique(X, return_inverse=True, axis=axis)
 
-    if state_values is None:
-        state_values = np.arange(max(index_series)+1)
-
-    k = len(index_series)
     n = len(state_values)
-
-    state_values = np.asarray(state_values)
-
-    # Compute the number of visits to each state and the frequency of
-    # transiting from state i to state
-    state_counter = np.zeros(n, dtype=int)
-    trans_counter = np.zeros((n, n), dtype=int)
-    _fill_counters(state_counter, trans_counter, index_series)
-
-    # Cut states where the column sum of trans_counter is zero (i.e.,
-    # inaccesible states according to the simulation)
-    zero_index = np.where(np.sum(trans_counter, axis=0) == 0)
-    trans_counter = np.delete(trans_counter, zero_index, axis=0)
-    trans_counter = np.delete(trans_counter, zero_index, axis=1)
-    state_values = np.delete(state_values, zero_index)
-
-    P = trans_counter / trans_counter.sum(1)[:, np.newaxis]
+    P = np.zeros((n, n))  # dtype=float to modify in place upon normalization
+    P = _count_transition_frequencies(indices, P)
+    P /= P.sum(1)[:, np.newaxis]
 
     mc = MarkovChain(P, state_values=state_values)
     return mc
 
-@njit
-def _fill_counters(state_counter, trans_counter, index_series):
+
+@njit(cache=True)
+def _count_transition_frequencies(index_series, trans_counter):
+    T = len(index_series)
     i = index_series[0]
-    for j in index_series[1:]:
-        state_counter[i] += 1
+    for t in range(1, T):
+        j = index_series[t]
         trans_counter[i, j] += 1
         i = j
+    return trans_counter
--- quantecon/markov/tests/test_estimate.py
+++ quantecon/markov/tests/test_estimate.py
@@ -6,29 +6,48 @@ import numpy as np
 from numpy.testing import assert_array_equal, assert_allclose
 from ..estimate import estimate_mc_discrete
 
-def test_est_integer_state():
-    test_series = (0, 1, 1, 1, 1, 0, 2, 1)
-    input_state_values = (0, 1, 2, 3)
-    P = [[0.,   0.5,  0.5 ],
-         [0.25, 0.75, 0.  ],
-         [0.,   1.,   0.  ]]
-    P = np.asarray(P)
-    final_state_values = np.array([0, 1, 2])
 
-    mc = estimate_mc_discrete(test_series, input_state_values)
-    assert_allclose(mc.P, P)
-    assert_array_equal(mc.state_values, final_state_values)
+class TestEstimateMCDiscrete:
+    def setup_method(self):
+        self.test_index_series = np.array((0, 1, 1, 1, 1, 0, 2, 1))
+        self.initial_state_values = np.array(['a', 'b', 'c', 'd'])
+        self.test_value_series = \
+            self.initial_state_values[self.test_index_series]
 
+        self.P = np.array([[0.,   0.5,  0.5 ],
+                           [0.25, 0.75, 0.  ],
+                           [0.,   1.,   0.  ]])
 
-def test_est_non_integer_state():
-    test_series = (0, 1, 1, 1, 1, 0, 2, 0)
-    input_state_values = ('a', 'b', 'c', 'd')
-    P = [[0.,   0.5,  0.5 ],
-         [0.25, 0.75, 0.  ],
-         [1.,   0.,   0.  ]]
-    P = np.asarray(P)
-    final_state_values = np.array(('a', 'b', 'c'))
+        self.final_state_indices = np.array([0, 1, 2])
+        self.final_state_values = \
+            self.initial_state_values[self.final_state_indices]
 
-    mc = estimate_mc_discrete(test_series, input_state_values)
-    assert_allclose(mc.P, P)
-    assert_array_equal(mc.state_values, final_state_values)
+    def test_integer_state(self):
+        mc = estimate_mc_discrete(self.test_index_series)
+        assert_allclose(mc.P, self.P)
+        assert_array_equal(mc.state_values, self.final_state_indices)
+
+    def test_non_integer_state(self):
+        mc = estimate_mc_discrete(self.test_value_series)
+        assert_allclose(mc.P, self.P)
+        assert_array_equal(mc.state_values, self.final_state_values)
+
+        mc = estimate_mc_discrete(self.test_index_series)
+        mc.state_values = self.initial_state_values[mc.state_values]
+        assert_allclose(mc.P, self.P)
+        assert_array_equal(mc.state_values, self.final_state_values)
+
+    def test_mult_dim_state(self):
+        initial_state_values = np.array([[0.97097089, 0.76167618],
+                                         [0.61878456, 0.41691572],
+                                         [0.42137226, 0.09307409],
+                                         [0.62696609, 0.40898893]])
+        X = initial_state_values[self.test_index_series]
+        ind = np.lexsort(
+            np.rot90(initial_state_values[self.final_state_indices])
+        )
+        final_state_values = initial_state_values[ind]
+
+        mc = estimate_mc_discrete(X)
+        assert_allclose(mc.P, self.P[np.ix_(ind, ind)])
+        assert_array_equal(mc.state_values, final_state_values)
