b7f4497860c1b6f18871ef568066cd5d13e802b2
Humphrey Yang <u6474961@anu.edu.au>
2023-02-16 12:00:08 +1100

    add tests for non-square C

--- quantecon/markov/tests/test_approximation.py
+++ quantecon/markov/tests/test_approximation.py
@@ -113,15 +113,16 @@ class TestDiscreteVar:
         self.random_state = np.random.RandomState(0)
 
         Omega = np.array([[0.0012346, -0.0000776],
-                              [-0.0000776, 0.0000401]])
+                          [-0.0000776, 0.0000401]])
         self.A = [[0.7901, -1.3570], 
-             [-0.0104, 0.8638]]
+                  [-0.0104, 0.8638]]
         self.C = sp.linalg.sqrtm(Omega)
         self.T= 1_000_000
         self.sizes = np.array((2, 3))
 
         # Expected outputs
-        self.S_out = [[-0.38556417, -0.05387746],
+        self.S_out = [
+                     [-0.38556417, -0.05387746],
                      [-0.38556417,  0.        ],
                      [-0.38556417,  0.05387746],
                      [ 0.38556417, -0.05387746],
@@ -129,12 +130,12 @@ class TestDiscreteVar:
                      [ 0.38556417,  0.05387746]]
 
         self.S_out_orderF =[
-                  [-0.38556417, -0.05387746],
-                  [ 0.38556417, -0.05387746],
-                  [-0.38556417,  0.        ],
-                  [ 0.38556417,  0.        ],
-                  [-0.38556417,  0.05387746],
-                  [ 0.38556417,  0.05387746]]
+                     [-0.38556417, -0.05387746],
+                     [ 0.38556417, -0.05387746],
+                     [-0.38556417,  0.        ],
+                     [ 0.38556417,  0.        ],
+                     [-0.38556417,  0.05387746],
+                     [ 0.38556417,  0.05387746]]
             
         self.P_out = [
             [1.61290323e-02, 1.12903226e-01, 0.00000000e+00, 3.70967742e-01, 
@@ -163,9 +164,26 @@ class TestDiscreteVar:
             6.97266822e-01, 5.25274456e-05],
             [0.00000000e+00, 0.00000000e+00, 4.45945946e-01, 1.89189189e-01, 
             3.37837838e-01, 2.70270270e-02]]
+        
+        self.P_out_non_square = [
+            [3.70370370e-02, 2.22222222e-01, 0.00000000e+00, 4.25925926e-01, 
+            3.14814815e-01, 0.00000000e+00],
+            [6.92865939e-05, 8.50870664e-01, 3.83177215e-02, 4.17954615e-04, 
+            1.10275202e-01, 4.91711312e-05],
+            [0.00000000e+00, 3.08160000e-01, 6.91360000e-01, 0.00000000e+00, 
+            3.91111111e-04, 8.88888889e-05],
+            [1.08405001e-04, 5.05890005e-04, 0.00000000e+00, 6.95707162e-01, 
+            3.03678543e-01, 0.00000000e+00],
+            [3.40242525e-05, 1.11864937e-01, 4.44583566e-04, 3.77260912e-02, 
+            8.49844169e-01, 8.61947730e-05],
+            [0.00000000e+00, 4.70588235e-01, 3.08823529e-01, 0.00000000e+00, 
+            1.76470588e-01, 4.41176471e-02]]
 
-        self.A, self.C, self.S_out, self.P_out = map(np.array,
-                                (self.A, self.C, self.S_out, self.P_out))
+        self.A, self.C, self.S_out, self.P_out, self.S_out_orderF,\
+        self.P_out_orderF, self.P_out_non_square \
+            = map(np.array,(self.A, self.C, self.S_out, self.P_out, 
+                                self.S_out_orderF, self.P_out_orderF, 
+                                self.P_out_non_square))
 
     def teardown_method(self):
         del self.A, self.C, self.S_out, self.P_out
@@ -196,3 +214,15 @@ class TestDiscreteVar:
                 random_state=self.random_state)
         assert_allclose(mc.state_values, self.S_out_orderF)
         assert_allclose(mc.P, self.P_out_orderF)
+
+    def test_order_non_squareC(self):
+        new_col = np.array([0, 0])
+        self.C = np.insert(self.C, 2, new_col, axis=1)
+        mc = discrete_var(
+            self.A, self.C, 
+            grid_sizes=self.sizes,
+            sim_length=self.T, 
+            order='C',
+            random_state=self.random_state)
+        assert_allclose(mc.state_values, self.S_out)
+        assert_allclose(mc.P, self.P_out_non_square)
