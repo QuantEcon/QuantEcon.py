59b3bc97c17ed57e333d8bd79e9d2bec826479bc
HengCheng <79777246+2789372130@users.noreply.github.com>
2023-03-06 14:06:51 +1100

    remove broken links

--- quantecon/bld.bat
+++ quantecon/bld.bat
@@ -3,6 +3,3 @@ if errorlevel 1 exit 1
 
 :: Add more build steps here, if they are necessary.
 
-:: See
-:: http://docs.continuum.io/conda/build.html
-:: for a list of environment variables that are set during the build process.
