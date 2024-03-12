63b80dbbbb8db651b70f010e68211d2ef3b3101a
Daisuke Oyama <oyama@e.u-tokyo.ac.jp>
2024-03-12 14:36:11 +0900

    RFC: linprog_simplex: Separate out Phase 1

--- quantecon/optimize/linprog_simplex.py
+++ quantecon/optimize/linprog_simplex.py
@@ -163,14 +163,9 @@ def linprog_simplex(c, A_ub=np.empty((0, 0)), b_ub=np.empty((0,)),
 
     # Phase 1
     success, status, num_iter_1 = \
-        solve_tableau(tableau, basis, max_iter, skip_aux=False,
-                      piv_options=piv_options)
+        solve_phase_1(tableau, basis, max_iter, piv_options=piv_options)
     num_iter += num_iter_1
-    if not success:  # max_iter exceeded
-        return SimplexResult(x, lambd, fun, success, status, num_iter)
-    if tableau[-1, -1] > piv_options.fea_tol:  # Infeasible
-        success = False
-        status = 2
+    if not success:
         return SimplexResult(x, lambd, fun, success, status, num_iter)
 
     # Modify the criterion row for Phase 2
@@ -440,6 +435,32 @@ def solve_tableau(tableau, basis, max_iter=10**6, skip_aux=True,
         basis[pivrow] = pivcol
 
     return success, status, num_iter
+
+
+@jit(nopython=True, cache=True)
+def solve_phase_1(tableau, basis, max_iter=10**6, piv_options=PivOptions()):
+    """
+    Perform the simplex algorithm for Phase 1 on a given tableau in
+    canonical form, by calling `solve_tableau` with `skip_aux=False`.
+
+    Parameters
+    ----------
+    See `solve_tableau`.
+
+    Returns
+    -------
+    See `solve_tableau`.
+
+    """
+    success, status, num_iter_1 = \
+        solve_tableau(tableau, basis, max_iter, skip_aux=False,
+                      piv_options=piv_options)
+
+    if success and tableau[-1, -1] > piv_options.fea_tol:  # Infeasible
+        success = False
+        status = 2
+
+    return success, status, num_iter_1
 
 
 @jit(nopython=True, cache=True)
