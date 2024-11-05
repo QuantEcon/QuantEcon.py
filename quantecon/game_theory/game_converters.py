80b4236455f3286f13b4c52c2304d49a92e0688b
Gautam <1gautamj@gmail.com>
2024-11-05 13:58:39 +0000

    load games from gam files. tests for lcp hoswon lover on polymatrix games.

---
+++ quantecon/game_theory/game_converters.py
@@ -0,0 +1,33 @@
+from .normal_form_game import NormalFormGame
+from itertools import product
+
+def qe_nfg_from_gam_file(filename):
+      
+    with open(filename, 'r') as file:
+        lines = file.readlines()
+        combined = [
+            token
+            for line in lines
+            for token in line.split()
+        ]
+
+        i = iter(combined)
+        players = int(next(i))
+        actions = [int(next(i)) for _ in range(players)]
+        
+        nfg = NormalFormGame(actions)
+
+        entries = [
+                {
+                    tuple(reversed(action_combination)): float(next(i))
+                    for action_combination in product(*[range(a) for a in actions])
+                }
+                for _ in range(players)
+            ]
+
+        for action_combination in product(*[range(a) for a in actions]):
+            nfg[tuple(reversed(action_combination))] = [
+                entries[p][action_combination] for p in range(players)
+                ]
+
+    return nfg
---
+++ quantecon/game_theory/tests/test_howson_lcp.py
@@ -0,0 +1,25 @@
+"""
+Tests for howson_lcp.py
+"""
+import numpy as np
+from numpy.testing import assert_allclose, assert_, assert_raises
+from quantecon.game_theory.game_converters import qe_nfg_from_gam_file
+from quantecon.game_theory.howson_lcp import polym_lcp_solver
+from quantecon.game_theory.polymatrix_game import PolymatrixGame
+
+
+def test_polym_lcp_solver_where_solution_is_pure_NE():
+    filename = "gam_files/big_polym.gam"
+    nfg = qe_nfg_from_gam_file(filename)
+    polym = PolymatrixGame.from_nf(nfg)
+    ne = polym_lcp_solver(polym)
+    worked = nfg.is_nash(ne)
+    assert_(worked)
+
+def test_polym_lcp_solver_where_lcp_solver_must_backtrack():
+    filename = "gam_files/triggers_back_case.gam"
+    nfg = qe_nfg_from_gam_file(filename)
+    polym = PolymatrixGame.from_nf(nfg)
+    ne = polym_lcp_solver(polym)
+    worked = nfg.is_nash(ne)
+    assert_(worked)
