79f74cb458e8f892f6c077b6a42d8857f8b0d23c
Daisuke Oyama <oyama@e.u-tokyo.ac.jp>
2024-11-06 18:08:08 +0900

    Get data dir in game_theory/tests
    (cherry picked from commit 82e4ebac7ed0c24665f44b5b1be1618bf10d3677)

--- quantecon/game_theory/tests/test_howson_lcp.py
+++ quantecon/game_theory/tests/test_howson_lcp.py
@@ -7,10 +7,18 @@ from quantecon.game_theory.game_converters import qe_nfg_from_gam_file
 from quantecon.game_theory.howson_lcp import polym_lcp_solver
 from quantecon.game_theory.polymatrix_game import PolymatrixGame
 
+import os
+
+
+# Mimicing quantecon.tests.util.get_data_dir
+data_dir_name = "gam_files"
+this_dir = os.path.dirname(__file__)
+data_dir = os.path.join(this_dir, data_dir_name)
+
 
 def test_polym_lcp_solver_where_solution_is_pure_NE():
-    filename = "gam_files/big_polym.gam"
-    nfg = qe_nfg_from_gam_file(filename)
+    filename = "big_polym.gam"
+    nfg = qe_nfg_from_gam_file(os.path.join(data_dir, filename))
     polym = PolymatrixGame.from_nf(nfg)
     ne = polym_lcp_solver(polym)
     worked = nfg.is_nash(ne)
@@ -18,8 +26,8 @@ def test_polym_lcp_solver_where_solution_is_pure_NE():
 
 
 def test_polym_lcp_solver_where_lcp_solver_must_backtrack():
-    filename = "gam_files/triggers_back_case.gam"
-    nfg = qe_nfg_from_gam_file(filename)
+    filename = "triggers_back_case.gam"
+    nfg = qe_nfg_from_gam_file(os.path.join(data_dir, filename))
     polym = PolymatrixGame.from_nf(nfg)
     ne = polym_lcp_solver(polym)
     worked = nfg.is_nash(ne)
