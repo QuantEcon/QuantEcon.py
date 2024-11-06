030ba94c49a5c2811d9ef6f6edef355dd2ae62a1
Daisuke Oyama <oyama@e.u-tokyo.ac.jp>
2024-11-06 14:58:46 +0900

    Register functions to be exported in `game_theory/__init__.py`

--- quantecon/game_theory/__init__.py
+++ quantecon/game_theory/__init__.py
@@ -4,6 +4,8 @@ Game Theory SubPackage
 
 """
 from .normal_form_game import Player, NormalFormGame
+from .polymatrix_game import PolymatrixGame
+
 from .normal_form_game import pure2mixed, best_response_2p
 from .random import (
     random_game, covariance_game, random_pure_actions, random_mixed_actions
@@ -13,6 +15,8 @@ from .support_enumeration import support_enumeration, support_enumeration_gen
 from .lemke_howson import lemke_howson
 from .mclennan_tourky import mclennan_tourky
 from .vertex_enumeration import vertex_enumeration, vertex_enumeration_gen
+from .howson_lcp import polym_lcp_solver
+
 from .game_generators import (
     blotto_game, ranking_game, sgc_game, tournament_game, unit_vector_game
 )
@@ -21,3 +25,5 @@ from .fictplay import FictitiousPlay, StochasticFictitiousPlay
 from .localint import LocalInteraction
 from .brd import BRD, KMR, SamplingBRD
 from .logitdyn import LogitDynamics
+
+from .game_converters import qe_nfg_from_gam_file
