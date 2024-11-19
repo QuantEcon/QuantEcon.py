bde2a24f5e56c4db107681aa2d55f0b675687709
Gautam <1gautamj@gmail.com>
2024-11-19 12:25:11 +0000

    adds long manually generated test case.

--- quantecon/game_theory/tests/test_howson_lcp.py
+++ quantecon/game_theory/tests/test_howson_lcp.py
@@ -56,7 +56,6 @@ def test_solves_rock_paper_scissors():
     nfg = polymg.to_nf()
     ne = polym_lcp_solver(polymg)
     worked = nfg.is_nash(ne)
-    print(ne)
     assert_(worked)
 
 
@@ -67,31 +66,31 @@ def test_solves_rps_with_scissorless_third_player():
     """
     polymatrix = {
         (0, 1): np.array([
-            [  0, -10,  10],
-            [ 10,   0, -10],
+            [0, -10,  10],
+            [10,   0, -10],
             [-10,  10,   0]
         ]),
         (0, 2): np.array([
-            [  0, -10],
-            [ 15,   0],
+            [0, -10],
+            [15,   0],
             [-10,   0]
         ]),
         (1, 0): np.array([
-            [  0, -10,  10],
-            [ 10,   0, -10],
+            [0, -10,  10],
+            [10,   0, -10],
             [-10,  10,   0]
         ]),
         (1, 2): np.array([
-            [  0, -10],
-            [ 15,   0],
+            [0, -10],
+            [15,   0],
             [-10,   0]
         ]),
         (2, 0): np.array([
-            [ 0, -10,  10],
+            [0, -10,  10],
             [10,   0, -10],
         ]),
         (2, 1): np.array([
-            [ 0, -10,  10],
+            [0, -10,  10],
             [10,   0, -10],
         ])
     }
@@ -103,7 +102,6 @@ def test_solves_rps_with_scissorless_third_player():
     nfg = polymg.to_nf()
     ne = polym_lcp_solver(polymg)
     worked = nfg.is_nash(ne)
-    print(ne)
     assert_(worked)
 
 
@@ -114,34 +112,34 @@ def test_solves_rps_with_rocking_third_player():
     """
     polymatrix = {
         (0, 1): np.array([
-            [  0, -10,  10],
-            [ 10,   0, -10],
+            [0, -10,  10],
+            [10,   0, -10],
             [-10,  10,   0]
         ]),
         (0, 2): np.array([
-            [  0, -10,   0],
-            [ 15,   0, -10],
+            [0, -10,   0],
+            [15,   0, -10],
             [-10,   0, -25]
         ]),
         (1, 0): np.array([
-            [  0, -10,  10],
-            [ 10,   0, -10],
+            [0, -10,  10],
+            [10,   0, -10],
             [-10,  10,   0]
         ]),
         (1, 2): np.array([
-            [  0, -10,   0],
-            [ 15,   0, -10],
+            [0, -10,   0],
+            [15,   0, -10],
             [-10,   0, -25]
         ]),
         (2, 0): np.array([
-            [ 0, -10,  10],
+            [0, -10,  10],
             [10,   0, -10],
-            [ 0, -15,  25]
+            [0, -15,  25]
         ]),
         (2, 1): np.array([
-            [ 0, -10,  10],
+            [0, -10,  10],
             [10,   0, -10],
-            [ 0, -15,  25]
+            [0, -15,  25]
         ])
     }
     polymg = PolymatrixGame(
@@ -152,5 +150,158 @@ def test_solves_rps_with_rocking_third_player():
     nfg = polymg.to_nf()
     ne = polym_lcp_solver(polymg)
     worked = nfg.is_nash(ne)
-    print(ne)
+    assert_(worked)
+
+
+def test_solves_multiplayer_rps_like():
+    """
+    The players and their actions are listed.
+    Schoolboy: Rock, Paper, Scissors.
+    Farmer: Chicken, Bull.
+    Zookeeper: Parrott, Monkey.
+    Curator: Mummy, Painting.
+    Archeologist: Mammoth, Pterradon, Tyrannosaurus.
+    Entomologist: Mosquito, Ant.
+    (The reasons for the payoffs can be found with imagination.)
+    """
+    polymatrix = {
+        (0, 1): np.array([
+            [2, 0],
+            [-5, 5],
+            [7, 0]
+        ]),
+        (0, 2): np.array([
+            [2, -2],
+            [-4, 5],
+            [4, -8]
+        ]),
+        (0, 3): np.array([
+            [1, -1],
+            [8, 2],
+            [6, 6]
+        ]),
+        (0, 4): np.array([
+            [0, 2, 0],
+            [2, 1, 1],
+            [1, 3, -5]
+        ]),
+        (0, 5): np.array([
+            [0, 2],
+            [6, 0],
+            [-2, -1]
+        ]),
+        (1, 0): np.array([
+            [0, 6, -6],
+            [0, -5, 0]
+        ]),
+        (1, 2): np.array([
+            [0, -5],
+            [-3, 5]
+        ]),
+        (1, 3): np.array([
+            [2, 4],
+            [2, 6]
+        ]),
+        (1, 4): np.array([
+            [0, -6, 0],
+            [0, -3, -9],
+        ]),
+        (1, 5): np.array([
+            [1, 1],
+            [-3, 0]
+        ]),
+        (2, 0): np.array([
+            [0, 5, -5],
+            [3, -6, 6],
+        ]),
+        (2, 1): np.array([
+            [0, 5],
+            [5, -4]
+        ]),
+        (2, 3): np.array([
+            [0, 3],
+            [-6, 5]
+        ]),
+        (2, 4): np.array([
+            [2, -3, 0],
+            [3, -4, -7]
+        ]),
+        (2, 5): np.array([
+            [1, 1],
+            [-3, 1]
+        ]),
+        (3, 0): np.array([
+            [1, -6, -8],
+            [0, 4, -8],
+        ]),
+        (3, 1): np.array([
+            [3, 0],
+            [-5, -6]
+        ]),
+        (3, 2): np.array([
+            [0, 4],
+            [-3, 3]
+        ]),
+        (3, 4): np.array([
+            [4, 1, -3],
+            [3, -2, -4]
+        ]),
+        (3, 5): np.array([
+            [0, -3],
+            [2, -3]
+        ]),
+        (4, 0): np.array([
+            [2, 0, -3],
+            [-1, 0, -3],
+            [2, 2, 2]
+        ]),
+        (4, 1): np.array([
+            [0, 0],
+            [3, 1],
+            [0, 9]
+        ]),
+        (4, 2): np.array([
+            [-1, -4],
+            [2, 5],
+            [0, 3]
+        ]),
+        (4, 3): np.array([
+            [-3, 0],
+            [-3, 2],
+            [-3, 2]
+        ]),
+        (4, 5): np.array([
+            [-2, 0],
+            [0, 0],
+            [0, 0]
+        ]),
+        (5, 0): np.array([
+            [5, -2, 5],
+            [4, 4, 4]
+        ]),
+        (5, 1): np.array([
+            [0, 5],
+            [0, 0]
+        ]),
+        (5, 2): np.array([
+            [0, 5],
+            [-2, -2]
+        ]),
+        (5, 3): np.array([
+            [0, 0],
+            [2, 2]
+        ]),
+        (5, 4): np.array([
+            [2, 0, 2],
+            [-1, 0, 1]
+        ])
+    }
+    polymg = PolymatrixGame(
+        6,
+        [3, 2, 2, 2, 3, 2],
+        polymatrix
+    )
+    nfg = polymg.to_nf()
+    ne = polym_lcp_solver(polymg)
+    worked = nfg.is_nash(ne, tol=1e-5)
     assert_(worked)
