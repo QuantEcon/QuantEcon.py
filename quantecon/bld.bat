19114c4d6881205fbc74a7f94865451c399ccbd2
kp992 <kpl992@outlook.com>
2025-01-04 18:25:09 -0800

    Remove unused files

--- quantecon/bld.bat
+++
@@ -1,8 +0,0 @@
-"%PYTHON%" setup.py install
-if errorlevel 1 exit 1
-
-:: Add more build steps here, if they are necessary.
-
-:: See 
-:: https://docs.conda.io/projects/conda-build/en/latest/resources/define-metadata.html
-:: for a list of environment variables that are set during the build process. 
--- quantecon/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-$PYTHON setup.py install
-
-# Add more build steps here, if they are necessary.
