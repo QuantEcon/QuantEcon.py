19abe5dbabc6524f6adfc3cd9d953c7100db4185
Matt McKay <mmcky@users.noreply.github.com>
2026-09-10 09:05:47 +1000

    Update quantecon/tests/test_lqnash.py
    Co-authored-by: QuantEcon <quantecon-services@users.noreply.github.com>

--- quantecon/tests/test_lqnash.py
+++ quantecon/tests/test_lqnash.py
@@ -94,7 +94,6 @@ class TestLQNash:
 
         # Define answers from matlab. TODO: hardcoding these
         # expected values is a fragile way to pin down the outputs
-        
         f1_ml = np.array([
                [0.243666582208565,   0.027236062661951, -6.827882928738190],
                [0.392370733875639,   0.139696450885998, -37.734107291009138]
