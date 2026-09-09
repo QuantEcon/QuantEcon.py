FIX: remove unused numerical locals (#923)
Signed-off-by: Emre K <110906681+kocaemre@users.noreply.github.com>

quantecon/_lss.py
@@ -391,7 +391,7 @@ class LinearStateSpace:
             The coefficients for y
         """
         # Pull out matrices
-        A, C, G, H = self.A, self.C, self.G, self.H
+        A, C, G = self.A, self.C, self.G
         Apower = np.copy(A)
 
         # Create room for coefficients